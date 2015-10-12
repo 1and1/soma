53bbf95ff936694e1459e1c0806cb1f080141420
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2015-10-12 14:51:20 +0200

    0.0.13: add more database grants
    * schema usage for role soma_svc
    * schema usage for role soma_inv
    * table grants for role soma_inv

--- database_create_sql_schema.go
+++ database_create_sql_schema.go
@@ -10,7 +10,7 @@ func createSqlSchema(printOnly bool, verbose bool) {
   queryMap := make( map[string]string )
   // slice storing the required statement order so foreign keys can
   // resolve successfully
-  queries := make( []string, 10 )
+  queries := make( []string, 20 )
 
 
   queryMap["createSchemaSoma"] = `create schema if not exists soma;`
@@ -43,6 +43,42 @@ func createSqlSchema(printOnly bool, verbose bool) {
 
   queryMap["grantServiceUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_svc;`
   queries[idx] = "grantServiceUserSchemaAuth"; idx++
+
+
+  queryMap["grantInventoryUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaSoma"; idx++
+
+
+  queryMap["grantInventoryUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaInventory"; idx++
+
+
+  queryMap["grantInventoryUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaAuth"; idx++
+
+
+  queryMap["grantUsageSomaSvcSoma"] = `grant usage on schema soma to soma_svc;`
+  queries[idx] = "grantUsageSomaSvcSoma"; idx++
+
+
+  queryMap["grantUsageSomaSvcInventory"] = `grant usage on schema inventory to soma_svc;`
+  queries[idx] = "grantUsageSomaSvcInventory"; idx++
+
+
+  queryMap["grantUsageSomaSvcAuth"] = `grant usage on schema auth to soma_svc;`
+  queries[idx] = "grantUsageSomaSvcAuth"; idx++
+
+
+  queryMap["grantUsageSomaInvSoma"] = `grant usage on schema soma to soma_inv;`
+  queries[idx] = "grantUsageSomaInvSoma"; idx++
+
+
+  queryMap["grantUsageSomaInvInventory"] = `grant usage on schema inventory to soma_inv;`
+  queries[idx] = "grantUsageSomaInvInventory"; idx++
+
+
+  queryMap["grantUsageSomaInvAuth"] = `grant usage on schema auth to soma_inv;`
+  queries[idx] = "grantUsageSomaInvAuth"; idx++
 
 
   performDatabaseTask( printOnly, verbose, queries, queryMap )
