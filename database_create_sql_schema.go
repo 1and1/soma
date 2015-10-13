36601b642edff37fdcb3036d2ff10df6938cb576
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2015-10-13 12:00:57 +0200

    0.0.14: reorder GRANT statements

--- database_create_sql_schema.go
+++ database_create_sql_schema.go
@@ -33,30 +33,6 @@ func createSqlSchema(printOnly bool, verbose bool) {
   queries[idx] = "alterDatabaseSearchPath"; idx++
 
 
-  queryMap["grantServiceUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_svc;`
-  queries[idx] = "grantServiceUserSchemaSoma"; idx++
-
-
-  queryMap["grantServiceUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_svc;`
-  queries[idx] = "grantServiceUserSchemaInventory"; idx++
-
-
-  queryMap["grantServiceUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_svc;`
-  queries[idx] = "grantServiceUserSchemaAuth"; idx++
-
-
-  queryMap["grantInventoryUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_inv;`
-  queries[idx] = "grantInventoryUserSchemaSoma"; idx++
-
-
-  queryMap["grantInventoryUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_inv;`
-  queries[idx] = "grantInventoryUserSchemaInventory"; idx++
-
-
-  queryMap["grantInventoryUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_inv;`
-  queries[idx] = "grantInventoryUserSchemaAuth"; idx++
-
-
   queryMap["grantUsageSomaSvcSoma"] = `grant usage on schema soma to soma_svc;`
   queries[idx] = "grantUsageSomaSvcSoma"; idx++
 
@@ -79,6 +55,30 @@ func createSqlSchema(printOnly bool, verbose bool) {
 
   queryMap["grantUsageSomaInvAuth"] = `grant usage on schema auth to soma_inv;`
   queries[idx] = "grantUsageSomaInvAuth"; idx++
+
+
+  queryMap["grantServiceUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_svc;`
+  queries[idx] = "grantServiceUserSchemaSoma"; idx++
+
+
+  queryMap["grantServiceUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_svc;`
+  queries[idx] = "grantServiceUserSchemaInventory"; idx++
+
+
+  queryMap["grantServiceUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_svc;`
+  queries[idx] = "grantServiceUserSchemaAuth"; idx++
+
+
+  queryMap["grantInventoryUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaSoma"; idx++
+
+
+  queryMap["grantInventoryUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaInventory"; idx++
+
+
+  queryMap["grantInventoryUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_inv;`
+  queries[idx] = "grantInventoryUserSchemaAuth"; idx++
 
 
   performDatabaseTask( printOnly, verbose, queries, queryMap )
