5a6876699342c1501279d73162d12f8f463f6b3b
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2015-10-14 11:49:24 +0200

    0.0.16: do not grant unnecessary permissions

--- database_grant_permissions.go
+++ database_grant_permissions.go
@@ -6,7 +6,7 @@ func grantPermissions(printOnly bool, verbose bool) {
   queryMap := make( map[string]string )
   // slice storing the required statement order so foreign keys can
   // resolve successfully
-  queries := make( []string, 6 )
+  queries := make( []string, 4 )
 
 
   queryMap["grantServiceUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_svc;`
@@ -21,16 +21,8 @@ func grantPermissions(printOnly bool, verbose bool) {
   queries[idx] = "grantServiceUserSchemaAuth"; idx++
 
 
-  queryMap["grantInventoryUserSchemaSoma"] = `grant select, insert, update, delete on all tables in schema soma to soma_inv;`
-  queries[idx] = "grantInventoryUserSchemaSoma"; idx++
-
-
   queryMap["grantInventoryUserSchemaInventory"] = `grant select, insert, update, delete on all tables in schema inventory to soma_inv;`
   queries[idx] = "grantInventoryUserSchemaInventory"; idx++
-
-
-  queryMap["grantInventoryUserSchemaAuth"] = `grant select, insert, update, delete on all tables in schema auth to soma_inv;`
-  queries[idx] = "grantInventoryUserSchemaAuth"; idx++
 
 
   performDatabaseTask( printOnly, verbose, queries, queryMap )
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
   app := cli.NewApp()
   app.Name = "somadbctl"
   app.Usage = "SOMA Database Control Utility"
-  app.Version = "0.0.15"
+  app.Version = "0.0.16"
 
   app = registerCommands(*app)
   app = registerFlags(*app)
