423d7dd742e217c2f5e4640ddc61677a439aac79
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2015-10-14 14:36:28 +0200

    Add vim modelines

--- views.go
+++ views.go
@@ -30,3 +30,5 @@ type ProtoViewDetails struct {
 	CreatedBy string   `json:"createdby,omitempty"`
 	UsedBy    []string `json:"usedby,omitempty"`
 }
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
