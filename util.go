ed408a4ce5da6890668db456414e00ba98fe0095
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2015-10-16 17:40:20 +0200

    Add more utility functions
    - getServerAssetIdByName(s string) uint64
      translates from server names to asset IDs
    
    - checkRestyResponse(r *resty.Response)
      checks the HTTP response status code, a 404 here for
      example means that an invalid api endpoint was
      called
    
    - decodeProtoResultServerFromResponse(resp *resty.Response) *somaproto.ProtoResultServer
      takes a resty HTTP response and returns the JSON response body
      decoded into a somaproto.ProtoResultServer struct

--- util.go
+++ util.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"gopkg.in/resty.v0"
 	"log"
 	"net/url"
 	"os"
@@ -39,3 +42,64 @@ func checkServerKeyword(s string) {
 		}
 	}
 }
+
+func getServerAssetIdByName(serverName string) uint64 {
+	url := getApiUrl()
+	url.Path = "/servers"
+
+	var req somaproto.ProtoRequestServer
+	var err error
+	req.Filter.Name = serverName
+	req.Filter.Online = true
+
+	resp, err := resty.New().
+		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
+		R().
+		SetBody(req).
+		Get(url.String())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		Slog.Fatal(err)
+	}
+
+	checkRestyResponse(resp)
+	serverResult := decodeProtoResultServerFromResponse(resp)
+
+	if len(serverResult.Servers) != 1 {
+		Slog.Fatal("Unexpected result set length - expected one server result")
+	}
+	if serverName != serverResult.Servers[0].Name {
+		Slog.Fatal("Received result set for incorrect server")
+	}
+	return serverResult.Servers[0].AssetId
+}
+
+func checkRestyResponse(resp *resty.Response) {
+	if resp.StatusCode() >= 400 {
+		fmt.Fprintf(os.Stderr, "Request error: %s\n", resp.Status())
+		Slog.Fatal(resp.Status())
+	}
+}
+
+func decodeProtoResultServerFromResponse(resp *resty.Response) *somaproto.ProtoResultServer {
+	decoder := json.NewDecoder(bytes.NewReader(resp.Body))
+	var res somaproto.ProtoResultServer
+	err := decoder.Decode(&res)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding server response body\n")
+		Slog.Printf("Error decoding server response body\n")
+		Slog.Fatal(err)
+	}
+	if res.Code > 299 {
+		fmt.Fprintf(os.Stderr, "Request failed: %d - %s\n",
+			res.Code, res.Status)
+		for _, e := range res.Text {
+			fmt.Fprintf(os.Stderr, "%s\n", e)
+			Slog.Printf("%s\n", e)
+		}
+		os.Exit(1)
+	}
+	return &res
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
