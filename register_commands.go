ce1e9a4a6271d446a986e55441df5d1f37c9203d
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2015-10-20 21:41:19 +0000

    Cleanup nonfunctional placeholders

--- register_commands.go
+++ register_commands.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/codegangsta/cli"
 )
 
@@ -14,48 +13,6 @@ func registerCommands(app cli.App) *cli.App {
 			Before: runtimePreCmd,
 			Action: cmdClientInit,
 		}, // end init
-		{
-			Name:   "servers",
-			Usage:  "subcommands for servers",
-			Before: runtimePreCmd,
-			Subcommands: []cli.Command{
-				{
-					Name:  "create",
-					Usage: "create a new physical server",
-					Action: func(c *cli.Context) {
-						fmt.Printf("servers/create\n")
-					},
-				},
-			},
-		},
-		{
-			Name:   "buckets",
-			Usage:  "subcommands for buckets",
-			Before: runtimePreCmd,
-			Subcommands: []cli.Command{
-				{
-					Name:  "create",
-					Usage: "create a new bucket",
-					Action: func(c *cli.Context) {
-						fmt.Printf("buckets/create\n")
-					},
-				},
-				{
-					Name:  "property",
-					Usage: "subcommands for bucket properties",
-					Subcommands: []cli.Command{
-						{
-							Name:        "add",
-							Usage:       "add a property",
-							Description: descBucketsPropertyAdd,
-							Action: func(c *cli.Context) {
-								fmt.Printf("buckets/property/add\n")
-							},
-						},
-					},
-				},
-			},
-		}, // end buckets
 		{
 			Name:   "views",
 			Usage:  "subcommands for views",
