7b44ed67d14231110d9253323bc019b849db4073
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2015-11-05 00:05:47 +0000

    Update structs for repositories

--- repository.go
+++ repository.go
@@ -5,7 +5,12 @@ import (
 )
 
 type ProtoRequestRepository struct {
-	Filter ProtoRepositoryFilter `json:"filter,omitempty"`
+	Repository ProtoRepository       `json:"repository,omitempty"`
+	Filter     ProtoRepositoryFilter `json:"filter,omitempty"`
+	Restore    bool                  `json:"restore,omitempty"`
+	Purge      bool                  `json:"purge,omitempty"`
+	Clear      bool                  `json:"clear,omitempty"`
+	Activate   bool                  `json:"activate,omitempty"`
 }
 
 type ProtoResultRepository struct {
@@ -13,15 +18,39 @@ type ProtoResultRepository struct {
 	Status       string            `json:"status,omitempty"`
 	Text         []string          `json:"text,omitempty"`
 	Repositories []ProtoRepository `json:"repositories,omitempty"`
+	JobId        uuid.UUID         `json:"jobid,omitempty"`
 }
 
 type ProtoRepository struct {
-	Id   uuid.UUID `json:"id,omitempty"`
-	Name string    `json:"name,omitempty"`
+	Id         uuid.UUID                 `json:"id,omitempty"`
+	Name       string                    `json:"name,omitempty"`
+	Team       string                    `json:"team,omitempty"`
+	IsDeleted  bool                      `json:"deleted,omitempty"`
+	IsActive   bool                      `json:"active,omitempty"`
+	Details    ProtoRepositoryDetails    `json:"details,omitempty"`
+	Properties []ProtoRepositoryProperty `json:"properties,omitempty"`
 }
 
 type ProtoRepositoryFilter struct {
-	Name string `json:"name,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Team      string `json:"team,omitempty"`
+	IsDeleted bool   `json:"deleted,omitempty"`
+	IsActive  bool   `json:"active,omitempty"`
+}
+
+type ProtoRepositoryDetails struct {
+	CreatedAt string `json:"createdat,omitempty"`
+	CreatedBy string `json:"createdby,omitempty"`
+}
+
+type ProtoRepositoryProperty struct {
+	Type         string `json:"type,omitempty"`
+	View         string `json:"view,omitempty"`
+	Property     string `json:"property,omitempty"`
+	Value        string `json:"value,omitempty"`
+	Inheritance  bool   `json:"inheritance,omitempty"`
+	ChildrenOnly bool   `json:"children,omitempty"`
+	Source       string `json:"source,omitempty"`
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
