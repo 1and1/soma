12a46cf2f90358dbc201c941bf43b01badcef04f
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-01-21 00:18:46 +0000

    Add structs for clusters

---
+++ cluster.go
@@ -0,0 +1,48 @@
+package somaproto
+
+type ProtoRequestCluster struct {
+	Cluster ProtoCluster       `json:"cluster,omitempty"`
+	Filter  ProtoClusterFilter `json:"filter,omitempty"`
+}
+
+type ProtoResultCluster struct {
+	Code     uint16         `json:"code,omitempty"`
+	Status   string         `json:"status,omitempty"`
+	Text     []string       `json:"text,omitempty"`
+	Clusters []ProtoCluster `json:"clusters,omitempty"`
+	JobId    string         `json:"jobid,omitempty"`
+}
+
+type ProtoCluster struct {
+	Id          string                 `json:"id,omitempty"`
+	Name        string                 `json:"name,omitempty"`
+	Bucket      string                 `json:"bucket,omitempty"`
+	BucketId    string                 `json:"bucketid,omitempty"`
+	ObjectState string                 `json:"objectstate,omitempty"`
+	Members     []ProtoNode            `json:"members,omitempty"`
+	Details     ProtoClusterDetails    `json:"details,omitempty"`
+	Properties  []ProtoClusterProperty `json:"properties,omitempty"`
+}
+
+type ProtoClusterFilter struct {
+	Name     string `json:"name,omitempty"`
+	Bucket   string `json:"bucket,omitempty"`
+	BucketId string `json:"bucketid,omitempty"`
+}
+
+type ProtoClusterDetails struct {
+	CreatedAt string `json:"createdat,omitempty"`
+	CreatedBy string `json:"createdby,omitempty"`
+}
+
+type ProtoClusterProperty struct {
+	Type         string `json:"type,omitempty"`
+	View         string `json:"view,omitempty"`
+	Property     string `json:"property,omitempty"`
+	Value        string `json:"value,omitempty"`
+	Inheritance  bool   `json:"inheritance,omitempty"`
+	ChildrenOnly bool   `json:"children,omitempty"`
+	Source       string `json:"source,omitempty"`
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
