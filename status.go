c7a92a6880e762b6b6e72e24553b7791894d7d4f
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-01-25 00:51:08 +0000

    Add structs for status

---
+++ status.go
@@ -0,0 +1,25 @@
+package somaproto
+
+type ProtoRequestStatus struct {
+	Status ProtoStatus `json:"status,omitempty"`
+}
+
+type ProtoResultStatus struct {
+	Code       uint16        `json:"code,omitempty"`
+	Status     string        `json:"status,omitempty"`
+	Text       []string      `json:"text,omitempty"`
+	StatusList []ProtoStatus `json:"statuslist,omitempty"`
+	JobId      string        `json:"jobid,omitempty"`
+}
+
+type ProtoStatus struct {
+	Status  string             `json:"status,omitempty"`
+	Details ProtoStatusDetails `json:"details,omitempty"`
+}
+
+type ProtoStatusDetails struct {
+	CreatedAt string `json:"createdat,omitempty"`
+	CreatedBy string `json:"createdby,omitempty"`
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
