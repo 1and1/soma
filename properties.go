a9f8cfcc7c9f0a91f328e75c3ddac6f8a6827dcb
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-01-31 22:38:53 +0000

    Update structs for properties

--- properties.go
+++ properties.go
@@ -1,14 +1,11 @@
 package somaproto
 
-import (
-	"github.com/satori/go.uuid"
-)
-
 type ProtoRequestProperty struct {
-	Custom  ProtoPropertyCustom  `json:"custom,omitempty"`
-	System  ProtoPropertySystem  `json:"system,omitempty"`
-	Service ProtoPropertyService `json:"service,omitempty"`
-	Filter  ProtoPropertyFilter  `json:"filter,omitempty"`
+	Custom  *ProtoPropertyCustom  `json:"custom,omitempty"`
+	System  *ProtoPropertySystem  `json:"system,omitempty"`
+	Service *ProtoPropertyService `json:"service,omitempty"`
+	Native  *ProtoPropertyNative  `json:"native,omitempty"`
+	Filter  *ProtoPropertyFilter  `json:"filter,omitempty"`
 }
 
 type ProtoResultProperty struct {
@@ -18,43 +15,49 @@ type ProtoResultProperty struct {
 	Custom  []ProtoPropertyCustom  `json:"custom,omitempty"`
 	System  []ProtoPropertySystem  `json:"system,omitempty"`
 	Service []ProtoPropertyService `json:"service,omitempty"`
+	Native  []ProtoPropertyNative  `json:"native,omitempty"`
+	JobId   string                 `json:"jobid,omitempty"`
 }
 
 type ProtoPropertyCustom struct {
-	Id         uuid.UUID `json:"id,omitempty"`
-	Repository string    `json:"repository,omitempty"`
-	Property   string    `json:"property,omitempty"`
-	Value      string    `json:"value,omitempty"`
+	Id         string `json:"id,omitempty"`
+	Repository string `json:"repository,omitempty"`
+	Property   string `json:"property,omitempty"`
+	Value      string `json:"value,omitempty"`
 }
 
 type ProtoPropertySystem struct {
-	Id       uuid.UUID `json:"id,omitempty"`
-	Property string    `json:"property,omitempty"`
-	Value    string    `json:"value,omitempty"`
+	Property string `json:"property,omitempty"`
+	Value    string `json:"value,omitempty"`
 }
 
 type ProtoPropertyService struct {
-	Id         uuid.UUID              `json:"id,omitempty"`
 	Property   string                 `json:"property,omitempty"`
 	Team       string                 `json:"team,omitempty"`
 	Attributes ProtoServiceAttributes `json:"attributes,omitempty"`
 }
 
+type ProtoPropertyNative struct {
+	Property string `json:"property,omitempty"`
+}
+
 type ProtoServiceAttributes struct {
-	Transport   []string `json:"proto_transport,omitempty"`
-	Application []string `json:"proto_application,omitempty"`
-	Port        []string `json:"port,omitempty"`
-	Process     []string `json:"process,omitempty"`
-	File        []string `json:"file,omitempty"`
-	Directory   []string `json:"directory,omitempty"`
-	Socket      []string `json:"socket,omitempty"`
-	Uid         []string `json:"uid,omitempty"`
-	Tls         string   `json:"tls,omitempty"`
-	Provider    string   `json:"provider,omitempty"`
+	ProtoTransport   []string `json:"proto_transport,omitempty"`
+	ProtoApplication []string `json:"proto_application,omitempty"`
+	Port             []string `json:"port,omitempty"`
+	ProcessComm      []string `json:"process_comm,omitempty"`
+	ProcessArgs      []string `json:"process_args,omitempty"`
+	FilePath         []string `json:"file_path,omitempty"`
+	DirectoryPath    []string `json:"directory_path,omitempty"`
+	UnixSocketPath   []string `json:"unix_socket_path,omitempty"`
+	Uid              []string `json:"uid,omitempty"`
+	Tls              []string `json:"tls,omitempty"`
+	SoftwareProvider []string `json:"software_provider,omitempty"`
 }
 
 type ProtoPropertyFilter struct {
-	Name string `json:"name,omitempty"`
+	Property string `json:"property,omitempty"`
+	Type     string `json:"type,omitempty"`
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
