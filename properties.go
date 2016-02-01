72847cde0858d2245e264d9b0fb00d45c9ad4e12
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-02-01 17:57:25 +0100

    Update structs for properties

--- properties.go
+++ properties.go
@@ -56,8 +56,58 @@ type ProtoServiceAttributes struct {
 }
 
 type ProtoPropertyFilter struct {
-	Property string `json:"property,omitempty"`
-	Type     string `json:"type,omitempty"`
+	Property   string `json:"property,omitempty"`
+	Type       string `json:"type,omitempty"`
+	Repository string `json:"type,omitempty"`
+}
+
+//
+func (p *ProtoResultProperty) ErrorMark(err error, imp bool, found bool,
+	length int) bool {
+	if p.markError(err) {
+		return true
+	}
+	if p.markImplemented(imp) {
+		return true
+	}
+	if p.markFound(found, length) {
+		return true
+	}
+	return p.markOk()
+}
+
+func (p *ProtoResultProperty) markError(err error) bool {
+	if err != nil {
+		p.Code = 500
+		p.Status = "ERROR"
+		p.Text = []string{err.Error()}
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultProperty) markImplemented(f bool) bool {
+	if f {
+		p.Code = 501
+		p.Status = "NOT IMPLEMENTED"
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultProperty) markFound(f bool, i int) bool {
+	if f || i == 0 {
+		p.Code = 404
+		p.Status = "NOT FOUND"
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultProperty) markOk() bool {
+	p.Code = 200
+	p.Status = "OK"
+	return false
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
