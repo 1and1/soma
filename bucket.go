b3aceb8233727cdb596fae3e70968fce9217510d
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-02-08 23:23:58 +0000

    Update bucket structs

--- bucket.go
+++ bucket.go
@@ -1,16 +1,12 @@
 package somaproto
 
-import (
-	"github.com/satori/go.uuid"
-)
-
 type ProtoRequestBucket struct {
-	Bucket  ProtoBucket       `json:"bucket,omitempty"`
-	Filter  ProtoBucketFilter `json:"filter,omitempty"`
-	Restore bool              `json:"restore,omitempty"`
-	Purge   bool              `json:"purge,omitempty"`
-	Freeze  bool              `json:"freeze,omitempty"`
-	Thaw    bool              `json:"thaw,omitempty"`
+	Bucket  *ProtoBucket       `json:"bucket,omitempty"`
+	Filter  *ProtoBucketFilter `json:"filter,omitempty"`
+	Restore bool               `json:"restore,omitempty"`
+	Purge   bool               `json:"purge,omitempty"`
+	Freeze  bool               `json:"freeze,omitempty"`
+	Thaw    bool               `json:"thaw,omitempty"`
 }
 
 type ProtoResultBucket struct {
@@ -18,20 +14,18 @@ type ProtoResultBucket struct {
 	Status  string        `json:"status,omitempty"`
 	Text    []string      `json:"text,omitempty"`
 	Buckets []ProtoBucket `json:"buckets,omitempty"`
-	JobId   uuid.UUID     `json:"jobid,omitempty"`
+	JobId   string        `json:"jobid,omitempty"`
 }
 
 type ProtoBucket struct {
-	Id           uuid.UUID           `json:"id,omitempty"`
-	Name         string              `json:"name,omitempty"`
-	Repository   string              `json:"repository,omitempty"`
-	RepositoryId string              `json:"repositoryid,omitempty"`
-	Team         string              `json:"team,omitempty"`
-	TeamId       string              `json:"team,omitempty"`
-	Environment  string              `json:"environment,omitempty"`
-	IsDeleted    bool                `json:"deleted,omitempty"`
-	IsFrozen     bool                `json:"frozen,omitempty"`
-	Details      *ProtoBucketDetails `json:"details,omitempty"`
+	Id          string              `json:"id,omitempty"`
+	Name        string              `json:"name,omitempty"`
+	Repository  string              `json:"repositoryid,omitempty"`
+	Team        string              `json:"team,omitempty"`
+	Environment string              `json:"environment,omitempty"`
+	IsDeleted   bool                `json:"deleted,omitempty"`
+	IsFrozen    bool                `json:"frozen,omitempty"`
+	Details     *ProtoBucketDetails `json:"details,omitempty"`
 	//	Properties []ProtoBucketProperty `json:"properties,omitempty"`
 }
 
@@ -46,6 +40,55 @@ type ProtoBucketFilter struct {
 type ProtoBucketDetails struct {
 	CreatedAt string `json:"createdat,omitempty"`
 	CreatedBy string `json:"createdby,omitempty"`
+}
+
+//
+func (p *ProtoResultBucket) ErrorMark(err error, imp bool, found bool,
+	length int) bool {
+	if p.markError(err) {
+		return true
+	}
+	if p.markImplemented(imp) {
+		return true
+	}
+	if p.markFound(found, length) {
+		return true
+	}
+	return p.markOk()
+}
+
+func (p *ProtoResultBucket) markError(err error) bool {
+	if err != nil {
+		p.Code = 500
+		p.Status = "ERROR"
+		p.Text = []string{err.Error()}
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultBucket) markImplemented(f bool) bool {
+	if f {
+		p.Code = 501
+		p.Status = "NOT IMPLEMENTED"
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultBucket) markFound(f bool, i int) bool {
+	if f || i == 0 {
+		p.Code = 404
+		p.Status = "NOT FOUND"
+		return true
+	}
+	return false
+}
+
+func (p *ProtoResultBucket) markOk() bool {
+	p.Code = 200
+	p.Status = "OK"
+	return false
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
