3f9e10e85cc1505749b2dfc192fba8ede56df871
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-03-04 17:42:51 +0100

    Start DeploymentDetails.DeepCompare

--- deployment_details.go
+++ deployment_details.go
@@ -25,4 +25,32 @@ type DeploymentDetails struct {
 	CheckInstance      *TreeCheckInstance    `json:"check_instance"`
 }
 
+func (dd *DeploymentDetails) DeepCompare(alternate *DeploymentDetails) bool {
+	if dd.Repository != alternate.Repository {
+		return false
+	}
+	if dd.Environment != alternate.Environment {
+		return false
+	}
+	if dd.Bucket != alternate.Bucket {
+		return false
+	}
+	if dd.ObjectType != alternate.ObjectType {
+		return false
+	}
+	if dd.View != alternate.View {
+		return false
+	}
+	if dd.Task != alternate.Task {
+		return false
+	}
+	if dd.Datacenter != alternate.Datacenter {
+		return false
+	}
+	//if !dd.Capability.DeepCompare(alternate.Capability) {
+	//	return false
+	//}
+	return true
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
