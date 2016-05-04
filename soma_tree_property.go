87c0322296a97cde5b4e12dfaf07222a169386a1
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-04 18:32:46 +0200

    Revert 5a92a20. Suspicion not confirmed

--- soma_tree_property.go
+++ soma_tree_property.go
@@ -1,8 +1,6 @@
 package somatree
 
 import (
-	"fmt"
-
 	"github.com/satori/go.uuid"
 )
 
@@ -407,7 +405,6 @@ func (p *PropertySystem) GetValue() string {
 
 func (p *PropertySystem) GetInstanceId(objType string, objId uuid.UUID) uuid.UUID {
 	if !uuid.Equal(p.Id, uuid.Nil) {
-		fmt.Println("LOL returning early")
 		return p.Id
 	}
 	for _, instance := range p.Instances {
