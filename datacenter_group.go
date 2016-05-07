9d84017ad0c58884c75e22883243f052c126dfb6
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-05-07 10:37:58 +0000

    Promote DatacenterGroup to full object

--- datacenter_group.go
+++ datacenter_group.go
@@ -1,34 +1,13 @@
 package somaproto
 
-type ProtoRequestDatacenterGroup struct {
-	DatacenterGroup string `json:"datacentergroup,omitempty"`
+type DatacenterGroup struct {
+	Name    string                  `json:"name, omitempty"`
+	Members *[]Datacenter           `json:"members, omitempty"`
+	Details *DatacenterGroupDetails `json:"details, omitempty"`
 }
 
-type ProtoResultDatacenterGroup struct {
-	Code   uint16   `json:"code,omitempty"`
-	Status string   `json:"status,omitempty"`
-	Text   []string `json:"text,omitempty"`
-}
-
-type ProtoResultDatacenterGroupList struct {
-	Code             uint16   `json:"code,omitempty"`
-	Status           string   `json:"status,omitempty"`
-	Text             []string `json:"text,omitempty"`
-	DatacenterGroups []string `json:"datacentergroups,omitempty"`
-}
-
-type ProtoResultDatacenterGroupDetail struct {
-	Code    uint16                      `json:"code,omitempty"`
-	Status  string                      `json:"status,omitempty"`
-	Text    []string                    `json:"text,omitempty"`
-	Details ProtoDatacenterGroupDetails `json:"details,omitempty"`
-}
-
-type ProtoDatacenterGroupDetails struct {
-	DatacenterGroup string   `json:"datacentergroup"`
-	CreatedAt       string   `json:"createdat,omitempty"`
-	CreatedBy       string   `json:"createdby,omitempty"`
-	UsedBy          []string `json:"usedby,omitempty"`
+type DatacenterGroupDetails struct {
+	DetailsCreation
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
--- request.go
+++ request.go
@@ -4,37 +4,38 @@ type Request struct {
 	Filter *Filter `json:"filter, omitempty"`
 	Flags  *Flags  `json:"flags, omitempty"`
 
-	Attribute      *Attribute      `json:"attribute, omitempty"`
-	Bucket         *Bucket         `json:"bucket, omitempty"`
-	Capability     *Capability     `json:"capability, omitempty"`
-	Category       *Category       `json:"category, omitempty"`
-	CheckConfig    *CheckConfig    `json:"checkConfig, omitempty"`
-	Cluster        *Cluster        `json:"cluster, omitempty"`
-	Datacenter     *Datacenter     `json:"datacenter, omitempty"`
-	Entity         *Entity         `json:"entity, omitempty"`
-	Environment    *Environment    `json:"environment, omitempty"`
-	Grant          *Grant          `json:"grant, omitempty"`
-	Group          *Group          `json:"group, omitempty"`
-	HostDeployment *HostDeployment `json:"hostDeployment, omitempty"`
-	Level          *Level          `json:"level, omitempty"`
-	Metric         *Metric         `json:"metric, omitempty"`
-	Mode           *Mode           `json:"mode, omitempty"`
-	Monitoring     *Monitoring     `json:"monitoring, omitempty"`
-	Node           *Node           `json:"node, omitempty"`
-	Oncall         *Oncall         `json:"oncall, omitempty"`
-	Permission     *Permission     `json:"permission, omitempty"`
-	Predicate      *Predicate      `json:"predicate, omitempty"`
-	Property       *Property       `json:"property, omitempty"`
-	Provider       *Provider       `json:"provider, omitempty"`
-	Repository     *Repository     `json:"repository, omitempty"`
-	Server         *Server         `json:"server, omitempty"`
-	State          *State          `json:"state, omitempty"`
-	Status         *Status         `json:"status, omitempty"`
-	Team           *Team           `json:"team, omitempty"`
-	Unit           *Unit           `json:"unit, omitempty"`
-	User           *User           `json:"user, omitempty"`
-	Validity       *Validity       `json:"validity, omitempty"`
-	View           *View           `json:"view, omitempty"`
+	Attribute       *Attribute       `json:"attribute, omitempty"`
+	Bucket          *Bucket          `json:"bucket, omitempty"`
+	Capability      *Capability      `json:"capability, omitempty"`
+	Category        *Category        `json:"category, omitempty"`
+	CheckConfig     *CheckConfig     `json:"checkConfig, omitempty"`
+	Cluster         *Cluster         `json:"cluster, omitempty"`
+	Datacenter      *Datacenter      `json:"datacenter, omitempty"`
+	DatacenterGroup *DatacenterGroup `json:"datacenterGroup, omitempty"`
+	Entity          *Entity          `json:"entity, omitempty"`
+	Environment     *Environment     `json:"environment, omitempty"`
+	Grant           *Grant           `json:"grant, omitempty"`
+	Group           *Group           `json:"group, omitempty"`
+	HostDeployment  *HostDeployment  `json:"hostDeployment, omitempty"`
+	Level           *Level           `json:"level, omitempty"`
+	Metric          *Metric          `json:"metric, omitempty"`
+	Mode            *Mode            `json:"mode, omitempty"`
+	Monitoring      *Monitoring      `json:"monitoring, omitempty"`
+	Node            *Node            `json:"node, omitempty"`
+	Oncall          *Oncall          `json:"oncall, omitempty"`
+	Permission      *Permission      `json:"permission, omitempty"`
+	Predicate       *Predicate       `json:"predicate, omitempty"`
+	Property        *Property        `json:"property, omitempty"`
+	Provider        *Provider        `json:"provider, omitempty"`
+	Repository      *Repository      `json:"repository, omitempty"`
+	Server          *Server          `json:"server, omitempty"`
+	State           *State           `json:"state, omitempty"`
+	Status          *Status          `json:"status, omitempty"`
+	Team            *Team            `json:"team, omitempty"`
+	Unit            *Unit            `json:"unit, omitempty"`
+	User            *User            `json:"user, omitempty"`
+	Validity        *Validity        `json:"validity, omitempty"`
+	View            *View            `json:"view, omitempty"`
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
