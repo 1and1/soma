c2ad696f2622bc060140dc65fd8cad354057526c
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-05-15 21:04:51 +0000

    Add DB schema version checks on startup
    Log excerpt:
    2016/05/15 21:02:11 Connected to database
    2016/05/15 21:02:11 DB Schema inventory, version: 201605060001
    2016/05/15 21:02:11 DB Schema root, version: 201605150001
    2016/05/15 21:02:11 DB Schema auth, version: 201605150002
    2016/05/15 21:02:11 DB Schema soma, version: 201605060001
    
    The follow cases will lead to application abort:
    - wrong schema version
    - missing required schema
    - extra unknown schema

--- database_connection.go
+++ database_connection.go
@@ -11,6 +11,10 @@ import (
 
 func connectToDatabase() {
 	var err error
+	var rows *sql.Rows
+	var schema string
+	var schemaVer int64
+
 	driver := "postgres"
 
 	connect := fmt.Sprintf("dbname='%s' user='%s' password='%s' host='%s' port='%s' sslmode='%s' connect_timeout='%s'",
@@ -36,6 +40,50 @@ func connectToDatabase() {
 	log.Print("Connected to database")
 	if _, err = conn.Exec(`SET TIME ZONE 'UTC';`); err != nil {
 		log.Fatal(err)
+	}
+
+	// required schema versions
+	required := map[string]int64{
+		"inventory": 201605060001,
+		"root":      201605150001,
+		"auth":      201605150002,
+		"soma":      201605060001,
+	}
+
+	if rows, err = conn.Query(`
+SELECT schema,
+       MAX(version) AS version
+FROM   public.schema_versions
+GROUP  BY schema;`); err != nil {
+		log.Fatal("Query db schema versions: ", err)
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&schema,
+			&schemaVer,
+		); err != nil {
+			log.Fatal("Schema check: ", err)
+		}
+		if rsv, ok := required[schema]; ok {
+			if rsv != required[schema] {
+				log.Fatal("Incompatible schema: ", schema, schemaVer)
+			} else {
+				log.Printf("DB Schema %s, version: %d", schema, schemaVer)
+				delete(required, schema)
+			}
+		} else {
+			log.Fatal("Unknown schema: ", schema)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal("Schema check: ", err)
+	}
+	if len(required) != 0 {
+		for s, _ := range required {
+			log.Printf("Missing schema: %s", s)
+		}
+		log.Fatal("FATAL - database incomplete")
 	}
 }
 
