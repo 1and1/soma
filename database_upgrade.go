bbabdad562c025ee45a666f3d639f3b14adb9056
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-05-16 13:32:56 +0000

    Hook up upgrade command to cli

--- database_upgrade.go
+++ database_upgrade.go
@@ -1,29 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
 const MaxInt = int(^uint(0) >> 1)
 
-var UpgradeVersions = map[string]map[int]func(int, string) int{
-	"inventory": map[int]func(int, string) int{
-		201605060001: mock_upgrade_inventory_201605060001,
-	},
-	"auth": map[int]func(int, string) int{
+var UpgradeVersions = map[string]map[int]func(int, string, bool) int{
+	//	"inventory": map[int]func(int, string) int{
+	//		201605060001: mock_upgrade_inventory_201605060001,
+	//	},
+	"auth": map[int]func(int, string, bool) int{
 		201605060001: upgrade_auth_to_201605150002,
 	},
-	"soma": map[int]func(int, string) int{
-		201605060001: mock_upgrade_soma_201605060001,
-		201605060002: mock_upgrade_soma_201605060002,
-	},
-	"root": map[int]func(int, string) int{
+	//	"soma": map[int]func(int, string) int{
+	//		201605060001: mock_upgrade_soma_201605060001,
+	//		201605060002: mock_upgrade_soma_201605060002,
+	//	},
+	"root": map[int]func(int, string, bool) int{
 		000000000001: install_root_201605150001,
+		201605150001: upgrade_root_201605160001,
 	},
 }
 
-func UpgradeSchema(target int, tool string) error {
+func commandUpgradeSchema(done chan bool, target int, tool string, printOnly bool) {
 	// no specific target specified => upgrade all the way
 	if target == 0 {
 		target = MaxInt
+	}
+	dbOpen()
+	if printOnly {
+		// in printOnly we have to close ourselve
+		defer db.Close()
 	}
 
 loop:
@@ -37,7 +47,7 @@ loop:
 		}
 
 		for f, ok := UpgradeVersions[schema][version]; ok; f, ok = UpgradeVersions[schema][version] {
-			version = f(version, tool)
+			version = f(version, tool, printOnly)
 			if version == 0 {
 				// something broke
 				// TODO: set error
@@ -48,77 +58,91 @@ loop:
 			}
 		}
 	}
-	return nil
+	done <- true
 }
 
-func mock_upgrade_inventory_201605060001(curr int, tool string) int {
+func upgrade_auth_to_201605150002(curr int, tool string, printOnly bool) int {
 	if curr != 201605060001 {
 		return 0
 	}
-	return 201605060002
-}
 
-func upgrade_auth_to_201605150002(curr int, tool string) int {
-	if curr != 201605060001 {
-		return 0
+	stmts := []string{
+		`DELETE FROM auth.user_token_authentication;`,
+		`ALTER TABLE auth.user_token_authentication ADD COLUMN salt varchar(256) NOT NULL;`,
+		`ALTER TABLE auth.user_token_authentication RENAME TO tokens;`,
+		`DROP TABLE auth.admin_token_authentication;`,
+		`ALTER TABLE auth.tools ADD CHECK( left( tool_name, 5 ) = 'tool_' );`,
+		`ALTER TABLE auth.user_authentication DROP COLUMN algorithm;`,
+		`ALTER TABLE auth.user_authentication DROP COLUMN rounds;`,
+		`ALTER TABLE auth.user_authentication DROP COLUMN salt;`,
+		`ALTER TABLE auth.admin_authentication DROP COLUMN algorithm;`,
+		`ALTER TABLE auth.admin_authentication DROP COLUMN rounds;`,
+		`ALTER TABLE auth.admin_authentication DROP COLUMN salt;`,
+		`ALTER TABLE auth.tool_authentication DROP COLUMN algorithm;`,
+		`ALTER TABLE auth.tool_authentication DROP COLUMN rounds;`,
+		`ALTER TABLE auth.tool_authentication DROP COLUMN salt;`,
 	}
-	dbOpen()
+	stmts = append(stmts,
+		fmt.Sprintf("INSERT INTO public.schema_versions (schema, version, description) VALUES ('auth', 201605150002, 'Upgrade - somadbctl %s');", tool),
+	)
+	for _, stmt := range stmts {
+		if printOnly {
+			fmt.Println(stmt)
+			continue
+		}
+		db.Exec(stmt)
+	}
 
-	db.Exec(`DELETE FROM auth.user_token_authentication;`)
-	db.Exec(`ALTER TABLE auth.user_token_authentication ADD COLUMN salt varchar(256) NOT NULL;`)
-	db.Exec(`ALTER TABLE auth.user_token_authentication RENAME TO tokens;`)
-	db.Exec(`DROP TABLE auth.admin_token_authentication;`)
-	db.Exec(`ALTER TABLE auth.tools ADD CHECK( left( tool_name, 5 ) = 'tool_' );`)
-	db.Exec(`ALTER TABLE auth.user_authentication DROP COLUMN algorithm;`)
-	db.Exec(`ALTER TABLE auth.user_authentication DROP COLUMN rounds;`)
-	db.Exec(`ALTER TABLE auth.user_authentication DROP COLUMN salt;`)
-	db.Exec(`ALTER TABLE auth.admin_authentication DROP COLUMN algorithm;`)
-	db.Exec(`ALTER TABLE auth.admin_authentication DROP COLUMN rounds;`)
-	db.Exec(`ALTER TABLE auth.admin_authentication DROP COLUMN salt;`)
-	db.Exec(`ALTER TABLE auth.tool_authentication DROP COLUMN algorithm;`)
-	db.Exec(`ALTER TABLE auth.tool_authentication DROP COLUMN rounds;`)
-	db.Exec(`ALTER TABLE auth.tool_authentication DROP COLUMN salt;`)
-	insert := fmt.Sprintf("INSERT INTO public.schema_versions (schema, version, description) VALUES ('auth', 201605150002, 'Upgrade - somadbctl %s');", tool)
-	db.Exec(insert)
 	return 201605150002
 }
 
-func install_root_201605150001(curr int, tool string) int {
+func install_root_201605150001(curr int, tool string, printOnly bool) int {
 	if curr != 000000000001 {
 		return 0
 	}
-	dbOpen()
 
-	db.Exec(`CREATE SCHEMA IF NOT EXISTS root;`)
-	db.Exec(`GRANT USAGE ON SCHEMA root TO soma_svc;`)
-	db.Exec(`CREATE TABLE IF NOT EXISTS root.token (token varchar(256) NOT NULL);`)
-	db.Exec(`CREATE TABLE IF NOT EXISTS root.flags (flag varchar(256) NOT NULL, status boolean NOT NULL DEFAULT 'no');`)
-	db.Exec(`GRANT SELECT ON ALL TABLES IN SCHEMA root TO soma_svc;`)
-	db.Exec(`INSERT INTO root.flags (flag, status) VALUES ('restricted', false), ('disabled', false);`)
-	insert := fmt.Sprintf("INSERT INTO public.schema_versions (schema, version, description) VALUES ('root', 201605150001, 'Upgrade create - somadbctl %s');", tool)
-	db.Exec(insert)
+	stmts := []string{
+		`CREATE SCHEMA IF NOT EXISTS root;`,
+		`GRANT USAGE ON SCHEMA root TO soma_svc;`,
+		`CREATE TABLE IF NOT EXISTS root.token (token varchar(256) NOT NULL);`,
+		`CREATE TABLE IF NOT EXISTS root.flags (flag varchar(256) NOT NULL, status boolean NOT NULL DEFAULT 'no');`,
+		`GRANT SELECT ON ALL TABLES IN SCHEMA root TO soma_svc;`,
+		`INSERT INTO root.flags (flag, status) VALUES ('restricted', false), ('disabled', false);`,
+	}
+	stmts = append(stmts,
+		fmt.Sprintf("INSERT INTO public.schema_versions (schema, version, description) VALUES ('root', 201605150001, 'Upgrade create - somadbctl %s');", tool),
+	)
+	for _, stmt := range stmts {
+		if printOnly {
+			fmt.Println(stmt)
+			continue
+		}
+		db.Exec(stmt)
+	}
 
 	return 201605150001
 }
 
-func mock_upgrade_soma_201605060001(curr int, tool string) int {
-	if curr != 201605060001 {
+func upgrade_root_201605160001(curr int, tool string, printOnly bool) int {
+	if curr != 201605150001 {
 		return 0
 	}
-	return 201605060002
-}
-
-func mock_upgrade_soma_201605060002(curr int, tool string) int {
-	if curr != 201605060002 {
-		return 0
-	}
-	return 201605060003
+	//return 201605150001
+	return 0
 }
 
 func getCurrentSchemaVersion(schema string) int {
-	// TODO: needs hook to mock current version to report for no-execute
-	//       case
-	return 201605060001
+	var (
+		version int
+		err     error
+	)
+	stmt := `SELECT MAX(version) AS version FROM public.schema_versions WHERE schema = $1::varchar GROUP BY schema;`
+	if err = db.QueryRow(stmt, schema).Scan(&version); err == sql.ErrNoRows {
+		return 000000000001
+	} else if err != nil {
+		log.Fatal(err)
+	}
+	return version
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
