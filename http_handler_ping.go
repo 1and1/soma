62d6cd5991001078aa2b33c5b420273d91b8689d
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-18 22:41:35 +0200

    Output mode (ro/rw) in HEAD

--- http_handler_ping.go
+++ http_handler_ping.go
@@ -11,6 +11,12 @@ func Ping(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 
 	w.Header().Set(`X-Powered-By`, `SOMA Configuration System`)
 	w.Header().Set(`X-Version`, SomaVersion)
+	switch SomaCfg.ReadOnly {
+	case true:
+		w.Header().Set(`X-SOMA-Mode`, `ReadOnly`)
+	case false:
+		w.Header().Set(`X-SOMA-Mode`, `Master`)
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
