22a7821a45f894932880643458f3b6cca08e8d4b
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-20 19:50:00 +0200

    Add key exchange protocol to adm lib

---
+++ lib/adm/kex.go
@@ -0,0 +1,44 @@
+package adm
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"gopkg.in/resty.v0"
+)
+
+func KeyExchange(c *resty.Client) (*auth.Kex, error) {
+	var (
+		err       error
+		kex, peer *auth.Kex
+		resp      *resty.Response
+	)
+
+	kex = auth.NewKex()
+	kex.SetTimeUTC()
+	if resp, err = c.R().SetBody(kex).Post(`/authenticate/`); err != nil {
+		goto fail
+	} else if resp.StatusCode() != 200 {
+		err = fmt.Errorf("Incorrect response code from SOMA, expected 200, got: %d",
+			resp.StatusCode())
+		goto fail
+	}
+
+	peer = &auth.Kex{}
+	if err = json.Unmarshal(resp.Body(), peer); err != nil {
+		goto fail
+	}
+
+	// store settings from peer
+	kex.SetPeerKey(peer.PublicKey())
+	if err = kex.SetRequestUUID(peer.Request.String()); err != nil {
+		goto fail
+	}
+
+	return kex, nil
+
+fail:
+	return nil, err
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
