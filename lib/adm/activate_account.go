7da7e8e119dad4c727ea911c681dd8c759663684
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-20 19:50:54 +0200

    Add account activation to adm lib

---
+++ lib/adm/activate_account.go
@@ -0,0 +1,59 @@
+package adm
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"gopkg.in/resty.v0"
+)
+
+func ActivateAccount(c *resty.Client, a *auth.Token) (*auth.Token, error) {
+	var (
+		kex  *auth.Kex
+		err  error
+		resp *resty.Response
+		body []byte
+	)
+	jBytes := &[]byte{}
+	cipher := &[]byte{}
+	plain := &[]byte{}
+	cred := &auth.Token{}
+
+	if *jBytes, err = json.Marshal(a); err != nil {
+		return nil, err
+	}
+
+	// establish key exchange for credential transmission
+	if kex, err = KeyExchange(c); err != nil {
+		return nil, err
+	}
+
+	// encrypt credentials
+	if err = kex.EncryptAndEncode(jBytes, cipher); err != nil {
+		return nil, err
+	}
+
+	// Send request
+	if resp, err = c.R().
+		SetHeader(`Content-Type`, `application/octet-stream`).
+		SetBody(*cipher).
+		Put(fmt.Sprintf(
+			"/authenticate/activate/%s", kex.Request.String())); err != nil {
+		return nil, err
+	} else if resp.StatusCode() != 200 {
+		return nil, fmt.Errorf("Activation failed with status code: %d", resp.StatusCode())
+	}
+
+	// decrypt reply
+	body = resp.Body()
+	if err = kex.DecodeAndDecrypt(&body, plain); err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(*plain, *cred); err != nil {
+		return nil, err
+	}
+
+	return cred, nil
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
