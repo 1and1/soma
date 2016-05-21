b22d67674920200ec2a2d3b7d7d7efafb368681f
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-05-21 13:38:44 +0000

    Only load credentials from active users

--- lib/stmt/supervisor_credentials.go
+++ lib/stmt/supervisor_credentials.go
@@ -14,12 +14,13 @@ SELECT aua.user_id,
        aua.reset_pending,
        aua.valid_from,
        aua.valid_until,
-	   iu.user_uid,
-	   iu.user_is_active
+       iu.user_uid
 FROM   inventory.users iu
 JOIN   auth.user_authentication aua
 ON     iu.user_id = aua.user_id
 WHERE  iu.user_id != '00000000-0000-0000-0000-000000000000'::uuid
-AND    NOW() < aua.valid_until;`
+AND    NOW() < aua.valid_until
+AND    NOT iu.user_is_deleted
+AND    iu.user_is_active;`
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
