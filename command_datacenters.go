7ae1792ff2724420a4f46d88c766b2f210484f2a
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-05-21 15:38:19 +0000

    Modernize command_datacenters, new runtime

--- command_datacenters.go
+++ command_datacenters.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/url"
 
 	"github.com/codegangsta/cli"
-	"gopkg.in/resty.v0"
 )
 
 func registerDatacenters(app cli.App) *cli.App {
@@ -42,26 +39,28 @@ func registerDatacenters(app cli.App) *cli.App {
 						Usage:  "Show information about a specific datacenter",
 						Action: runtime(cmdDatacentersShow),
 					},
-					{
-						Name:   "groupadd",
-						Usage:  "Add a datacenter to a datacenter group",
-						Action: runtime(cmdDatacentersAddToGroup),
-					},
-					{
-						Name:   "groupdel",
-						Usage:  "Remove a datacenter from a datacenter group",
-						Action: runtime(cmdDatacentersRemoveFromGroup),
-					},
-					{
-						Name:   "grouplist",
-						Usage:  "List all datacenter groups",
-						Action: runtime(cmdDatacentersListGroups),
-					},
-					{
-						Name:   "groupshow",
-						Usage:  "Show information about a datacenter group",
-						Action: runtime(cmdDatacentersShowGroup),
-					},
+					/*
+						{
+							Name:   "groupadd",
+							Usage:  "Add a datacenter to a datacenter group",
+							Action: runtime(cmdDatacentersAddToGroup),
+						},
+						{
+							Name:   "groupdel",
+							Usage:  "Remove a datacenter from a datacenter group",
+							Action: runtime(cmdDatacentersRemoveFromGroup),
+						},
+						{
+							Name:   "grouplist",
+							Usage:  "List all datacenter groups",
+							Action: runtime(cmdDatacentersListGroups),
+						},
+						{
+							Name:   "groupshow",
+							Usage:  "Show information about a datacenter group",
+							Action: runtime(cmdDatacentersShowGroup),
+						},
+					*/
 				},
 			}, // end datacenters
 		}...,
@@ -70,199 +69,139 @@ func registerDatacenters(app cli.App) *cli.App {
 }
 
 func cmdDatacentersAdd(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
-	url.Path = "/datacenters"
+	utl.ValidateCliArgumentCount(c, 1)
 
-	a := c.Args()
-	datacenter := a.First()
-	if datacenter == "" {
-		log.Fatal("Syntax error")
-	}
-	log.Printf("Command: add datacenter [%s]", datacenter)
+	req := proto.NewDatacenterRequest()
+	req.Datacenter.Locode = c.Args().First()
 
-	var req proto.Request
-	req.Datacenter = &proto.Datacenter{}
-	req.Datacenter.Locode = datacenter
-
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		SetBody(req).
-		Post(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s\n", resp.Status())
+	resp := utl.PostRequestWithBody(Client, req, `/datacenters/`)
+	fmt.Println(resp)
 	return nil
 }
 
 func cmdDatacentersAddToGroup(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
+	/*
+		url, err := url.Parse(Cfg.Api)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	a := c.Args()
-	// we expected exactly 3 arguments
-	if len(a) != 3 {
-		log.Fatal("Syntax error")
-	}
-	// second arg must be `to`
-	if a.Get(1) != "group" {
-		log.Fatal("Syntax error")
-	}
-	log.Printf("Command: add datacenter [%s] to group [%s]", a.Get(0), a.Get(2))
+		a := c.Args()
+		// we expected exactly 3 arguments
+		if len(a) != 3 {
+			log.Fatal("Syntax error")
+		}
+		// second arg must be `to`
+		if a.Get(1) != "group" {
+			log.Fatal("Syntax error")
+		}
+		log.Printf("Command: add datacenter [%s] to group [%s]", a.Get(0), a.Get(2))
 
-	var req proto.Request
-	req.Datacenter = &proto.Datacenter{}
-	req.Datacenter.Locode = a.Get(0)
-	url.Path = fmt.Sprintf("/datacentergroups/%s", a.Get(2))
+		var req proto.Request
+		req.Datacenter = &proto.Datacenter{}
+		req.Datacenter.Locode = a.Get(0)
+		url.Path = fmt.Sprintf("/datacentergroups/%s", a.Get(2))
 
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		SetBody(req).
-		Patch(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s", resp.Status())
+		resp, err := resty.New().
+			SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
+			R().
+			SetBody(req).
+			Patch(url.String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Response: %s", resp.Status())
+	*/
 	return nil
 }
 
 func cmdDatacentersRemove(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
+	utl.ValidateCliArgumentCount(c, 1)
 
-	a := c.Args()
-	datacenter := a.First()
-	if datacenter == "" {
-		log.Fatal("Syntax error")
-	}
-	log.Printf("Command: remove datacenter [%s]", datacenter)
-	url.Path = fmt.Sprintf("/datacenters/%s", datacenter)
+	path := fmt.Sprintf("/datacenters/%s", c.Args().First())
 
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		Delete(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s\n", resp.Status())
+	resp := utl.DeleteRequest(Client, path)
+	fmt.Println(resp)
 	return nil
 }
 
 func cmdDatacentersRemoveFromGroup(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
+	/*
+		url, err := url.Parse(Cfg.Api)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	a := c.Args()
-	// we expected exactly 3 arguments
-	if len(a) != 3 {
-		log.Fatal("Syntax error")
-	}
-	// second arg must be `to`
-	if a.Get(1) != "group" {
-		log.Fatal("Syntax error")
-	}
-	log.Printf("Command: remove datacenter [%s] from group [%s]", a.Get(0), a.Get(2))
+		a := c.Args()
+		// we expected exactly 3 arguments
+		if len(a) != 3 {
+			log.Fatal("Syntax error")
+		}
+		// second arg must be `to`
+		if a.Get(1) != "group" {
+			log.Fatal("Syntax error")
+		}
+		log.Printf("Command: remove datacenter [%s] from group [%s]", a.Get(0), a.Get(2))
 
-	var req proto.Request
-	req.Datacenter = &proto.Datacenter{}
-	req.Datacenter.Locode = a.Get(0)
-	url.Path = fmt.Sprintf("/datacentergroups/%s", a.Get(2))
+		var req proto.Request
+		req.Datacenter = &proto.Datacenter{}
+		req.Datacenter.Locode = a.Get(0)
+		url.Path = fmt.Sprintf("/datacentergroups/%s", a.Get(2))
 
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		SetBody(req).
-		Delete(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s", resp.Status())
+		resp, err := resty.New().
+			SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
+			R().
+			SetBody(req).
+			Delete(url.String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Response: %s", resp.Status())
+	*/
 	return nil
 }
 
 func cmdDatacentersRename(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
+	utl.ValidateCliArgumentCount(c, 3)
+	key := []string{`to`}
 
-	a := c.Args()
-	// we expected exactly 3 arguments
-	if len(a) != 3 {
-		log.Fatal("Syntax error")
-	}
-	// second arg must be `to`
-	if a.Get(1) != "to" {
-		log.Fatal("Syntax error")
-	}
-	log.Printf("Command: rename datacenter [%s] to [%s]", a.Get(0), a.Get(2))
+	opts := utl.ParseVariadicArguments(key, key, key, c.Args().Tail())
 
-	var req proto.Request
-	req.Datacenter = &proto.Datacenter{}
-	req.Datacenter.Locode = a.Get(2)
-	url.Path = fmt.Sprintf("/datacenters/%s", a.Get(0))
+	req := proto.NewDatacenterRequest()
+	req.Datacenter.Locode = opts[`to`][0]
 
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		SetBody(req).
-		Put(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s\n", resp.Status())
+	path := fmt.Sprintf("/datacenters/%s", c.Args().First())
+
+	resp := utl.PutRequestWithBody(Client, req, path)
+	fmt.Println(resp)
 	return nil
 }
 
 func cmdDatacentersList(c *cli.Context) error {
-	url, err := url.Parse(Cfg.Api)
-	if err != nil {
-		log.Fatal(err)
-	}
-	url.Path = "/datacenters"
-
-	a := c.Args()
-	if len(a) != 0 {
-		log.Fatal("Syntax error")
-	}
-
-	resp, err := resty.New().
-		SetRedirectPolicy(resty.FlexibleRedirectPolicy(3)).
-		R().
-		Get(url.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response: %s\n", resp.Status())
+	utl.ValidateCliArgumentCount(c, 0)
+	resp := utl.GetRequest(Client, `/datacenters/`)
+	fmt.Println(resp)
 	return nil
 }
 
 func cmdDatacentersListGroups(c *cli.Context) error {
-	utl.ValidateCliArgumentCount(c, 0)
+	/*
+		utl.ValidateCliArgumentCount(c, 0)
 
-	resp := utl.GetRequest(Client, "/datacentergroups/")
-	fmt.Println(resp)
+		resp := utl.GetRequest(Client, "/datacentergroups/")
+		fmt.Println(resp)
+	*/
 	return nil
 }
 
 func cmdDatacentersShowGroup(c *cli.Context) error {
-	utl.ValidateCliArgumentCount(c, 1)
+	/*
+		utl.ValidateCliArgumentCount(c, 1)
 
-	path := fmt.Sprintf("/datacentergroups/%s", c.Args().First())
-	resp := utl.GetRequest(Client, path)
-	fmt.Println(resp)
+		path := fmt.Sprintf("/datacentergroups/%s", c.Args().First())
+		resp := utl.GetRequest(Client, path)
+		fmt.Println(resp)
+	*/
 	return nil
 }
 
