10c95c684baae9df6ba746b760ed3cb85cb8bbe2
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-24 14:54:36 +0200

    Import extractAddress function

--- util.go
+++ util.go
@@ -194,4 +194,23 @@ func GetPropertyTypeFromUrl(u *url.URL) (string, error) {
 	}
 }
 
+// extractAddress extracts the IP address part of the IP:port string
+// set as net/http.Request.RemoteAddr. It handles IPv4 cases like
+// 192.0.2.1:48467 and IPv6 cases like [2001:db8::1%lo0]:48467
+func extractAddress(str string) string {
+	var addr string
+
+	switch {
+	case strings.Contains(str, `]`):
+		// IPv6 address [2001:db8::1%lo0]:48467
+		addr = strings.Split(str, `]`)[0]
+		addr = strings.Split(addr, `%`)[0]
+		addr = strings.TrimLeft(addr, `[`)
+	default:
+		// IPv4 address 192.0.2.1:48467
+		addr = strings.Split(str, `:`)[0]
+	}
+	return addr
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
