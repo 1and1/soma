697c3eb341b1d415b82038e0ec7a8c9d493b079f
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-05-24 15:55:33 +0200

    Add SQL statements for PermissionCategory

--- lib/stmt/supervisor_permission.go
+++ lib/stmt/supervisor_permission.go
@@ -10,4 +10,34 @@ SELECT permission_id,
        permission_name
 FROM   soma.permissions;`
 
+const AddPermissionCategory = `
+INSERT INTO soma.permission_types (
+            permission_type,
+            created_by
+)
+SELECT $1::varchar,
+       $2::uuid
+WHERE NOT EXISTS (
+      SELECT permission_type
+      FROM   soma.permission_types
+      WHERE  permission_type = $1::varchar
+);`
+
+const DeletePermissionCategory = `
+DELETE FROM soma.permission_types
+WHERE permission_type = $1::varchar;`
+
+const ListPermissionCategory = `
+SELECT spt.permission_type
+FROM   soma.permission_types spt:`
+
+const ShowPermissionCategory = `
+SELECT spt.permission_type,
+       iu.user_uid,
+       spt.created_by
+FROM   soma.permission_types spt
+JOIN   inventory.users iu
+ON     spt.created_by = iu.user_id
+WHERE  spt.permission_type = $1::varchar;`
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
