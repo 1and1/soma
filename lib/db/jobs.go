004b6b0d46b9fb248f0af4ddd3e9c677c539b2a1
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-06-03 17:18:48 +0200

    Update saving of JobIds

--- lib/db/jobs.go
+++ lib/db/jobs.go
@@ -8,13 +8,35 @@
 
 package db
 
-import "github.com/boltdb/bolt"
+import (
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
 
 func (d *DB) SaveJob(jid, jtype string) error {
+	now := time.Now().UTC().Format(rfc3339Milli)
+
 	return d.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(`jobs`)).Bucket([]byte(`active`))
-		return b.Put([]byte(jid), []byte(jtype))
+		id, _ := b.NextSequence()
+		return b.Put(
+			uitob(id),
+			[]byte(fmt.Sprintf("id:%s|time:%s|type:%s", jid, now, jtype)),
+		)
 	})
+}
+
+func uitob(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, v)
+	return b
+}
+
+func botui(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b)
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
