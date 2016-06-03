590218f3efb045b04a7aa53a18ca5f4010c7223e
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-06-03 18:25:23 +0200

    Add libdb.GetActiveJobs()

--- lib/db/jobs.go
+++ lib/db/jobs.go
@@ -11,12 +11,18 @@ package db
 import (
 	"encoding/binary"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/boltdb/bolt"
 )
 
 func (d *DB) SaveJob(jid, jtype string) error {
+	if err := d.Open(); err != nil {
+		return err
+	}
+	defer d.Close()
 	now := time.Now().UTC().Format(rfc3339Milli)
 
 	return d.db.Update(func(tx *bolt.Tx) error {
@@ -24,9 +30,41 @@ func (d *DB) SaveJob(jid, jtype string) error {
 		id, _ := b.NextSequence()
 		return b.Put(
 			uitob(id),
-			[]byte(fmt.Sprintf("id:%s|time:%s|type:%s", jid, now, jtype)),
+			[]byte(fmt.Sprintf("%s|%s|%s", jid, now, jtype)),
 		)
 	})
+}
+
+// Slice of slice of strings, with the inner slice contents as follows:
+// 0: storage key
+// 1: JobID
+// 2: Timestamp
+// 3: JobType
+func (d *DB) GetActiveJobs() ([][]string, error) {
+	if err := d.Open(); err != nil {
+		return nil, err
+	}
+	defer d.Close()
+
+	count := 0
+	jobs := [][]string{}
+	if err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(`jobs`)).Bucket([]byte(`active`))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			r := []string{bots(k)}
+			r = append(r, strings.Split(string(v), `|`)...)
+			jobs = append(jobs, r)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	if count > 0 {
+		return jobs, nil
+	}
+	return nil, bolt.ErrBucketNotFound
 }
 
 func uitob(v uint64) []byte {
@@ -37,6 +75,10 @@ func uitob(v uint64) []byte {
 
 func botui(b []byte) uint64 {
 	return binary.BigEndian.Uint64(b)
+}
+
+func bots(b []byte) string {
+	return strconv.FormatUint(binary.BigEndian.Uint64(b), 10)
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
