7cfd44cb7b6f380c1592ca47ecbc3361dc341aa6
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-06-12 21:22:57 +0000

    0.5.7: bump version

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "somaadm"
 	app.Usage = "SOMA Administrative Interface"
-	app.Version = "0.5.6"
+	app.Version = "0.5.7"
 	app.EnableBashCompletion = true
 
 	app = registerCommands(*app)
