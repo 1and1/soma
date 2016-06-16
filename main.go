cd7cc54369767c8f7ac74642833894d52efcf6c3
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-06-16 22:44:21 +0200

    Release v0.5.17, forgotten vendor update

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "somaadm"
 	app.Usage = "SOMA Administrative Interface"
-	app.Version = "0.5.16"
+	app.Version = "0.5.17"
 	app.EnableBashCompletion = true
 
 	app = registerCommands(*app)
