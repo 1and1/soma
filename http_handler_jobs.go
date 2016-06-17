b4d9e6795a335603f0ebd17dbfac3eeef01420fe
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-06-17 18:37:55 +0200

    Implement Jobs http handlers

--- http_handler_jobs.go
+++ http_handler_jobs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -26,31 +27,87 @@ func JobDelay(w http.ResponseWriter, r *http.Request,
 func ListJobs(w http.ResponseWriter, r *http.Request,
 	params httprouter.Params) {
 	defer PanicCatcher(w)
-	if ok, _ := IsAuthorized(params.ByName(`AuthenticatedUser`),
+	var ok, admin bool
+	if ok, admin = IsAuthorized(params.ByName(`AuthenticatedUser`),
 		`jobs_list`, ``, ``, ``); !ok {
 		DispatchForbidden(&w, nil)
 		return
 	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`jobs_r`].(jobsRead)
+	handler.input <- msg.Request{
+		Type:       `job`,
+		Action:     `list`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		IsAdmin:    admin,
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
 }
 
 func ShowJob(w http.ResponseWriter, r *http.Request,
 	params httprouter.Params) {
 	defer PanicCatcher(w)
-	if ok, _ := IsAuthorized(params.ByName(`AuthenticatedUser`),
+	var ok, admin bool
+	if ok, admin = IsAuthorized(params.ByName(`AuthenticatedUser`),
 		`jobs_show`, ``, ``, ``); !ok {
 		DispatchForbidden(&w, nil)
 		return
 	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`jobs_r`].(jobsRead)
+	handler.input <- msg.Request{
+		Type:       `job`,
+		Action:     `show`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		IsAdmin:    admin,
+		Job:        proto.Job{Id: params.ByName(`jobid`)},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
 }
 
 func SearchJob(w http.ResponseWriter, r *http.Request,
 	params httprouter.Params) {
 	defer PanicCatcher(w)
-	if ok, _ := IsAuthorized(params.ByName(`AuthenticatedUser`),
+	var ok, admin bool
+	if ok, admin = IsAuthorized(params.ByName(`AuthenticatedUser`),
 		`jobs_search`, ``, ``, ``); !ok {
 		DispatchForbidden(&w, nil)
 		return
 	}
+
+	cReq := proto.NewJobFilter()
+	err := DecodeJsonBody(r, &cReq)
+	if err != nil {
+		DispatchBadRequest(&w, err)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`jobs_r`].(jobsRead)
+	handler.input <- msg.Request{
+		Type:       `jobs`,
+		Action:     `search/idlist`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		IsAdmin:    admin,
+		Search: msg.Filter{
+			IsDetailed: cReq.Flags.Detailed,
+			Job: proto.JobFilter{
+				IdList: cReq.Filter.Job.IdList,
+			},
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
