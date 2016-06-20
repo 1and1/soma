3d97a20e08fd0f2bd13aac7c846dde27d6ee8899
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-06-20 21:41:11 +0000

    Update level helper

--- util/helper_level.go
+++ util/helper_level.go
@@ -1,25 +1,43 @@
 package util
 
 import (
+	"fmt"
+
 	"gopkg.in/resty.v0"
 )
 
 func (u *SomaUtil) TryGetLevelNameByNameOrShort(c *resty.Client, s string) string {
-	req := proto.Request{
-		Filter: &proto.Filter{
-			Level: &proto.LevelFilter{
-				Name:      s,
-				ShortName: s,
-			},
-		},
-	}
-	resp := u.PostRequestWithBody(c, req, "/filter/levels/")
-	levelResult := u.DecodeProtoResultLevelFromResponse(resp)
+	req := proto.NewLevelFilter()
+	req.Filter.Level.Name = s
+	req.Filter.Level.ShortName = s
 
-	if s != (*levelResult.Levels)[0].Name && s != (*levelResult.Levels)[0].ShortName {
-		u.Abort("Received result set for incorrect level")
+	resp, err := adm.PostReqBody(req, `/filter/levels/`)
+	if err != nil {
+		u.Abort(fmt.Sprintf("Level lookup request error: %s", err.Error()))
 	}
-	return (*levelResult.Levels)[0].Name
+	result, err := u.ResultFromResponse(resp)
+	if se, ok := err.(SomaError); ok {
+		if se.RequestError() {
+			u.Abort(fmt.Sprintf("Level lookup request error: %s", se.Error()))
+		}
+		if se.Code() == 404 {
+			u.Abort(fmt.Sprintf(
+				"Could not find notification level with name %s",
+				s,
+			))
+		}
+		u.Abort(fmt.Sprintf("Level lookup application error: %s", err.Error()))
+	}
+
+	if s != (*result.Levels)[0].Name && s != (*result.Levels)[0].ShortName {
+		u.Abort(fmt.Sprintf(
+			"Notification level lookup failed. Wanted %s, received %s/%s",
+			s,
+			(*result.Levels)[0].Name,
+			(*result.Levels)[0].ShortName,
+		))
+	}
+	return (*result.Levels)[0].Name
 }
 
 func (u *SomaUtil) DecodeProtoResultLevelFromResponse(resp *resty.Response) *proto.Result {
