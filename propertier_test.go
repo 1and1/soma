6ab105ee9a0f36c9e39fb58675bc6fee828ee2a2
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-06-25 17:53:37 +0000

    Test: DeleteProperty on inheritance=false
    This test also handles that .Parent.syncProperty() does not
    inherit down the inheritance=false property.

--- propertier_test.go
+++ propertier_test.go
@@ -651,6 +651,198 @@ func TestDeleteProperty(t *testing.T) {
 	}
 }
 
+func TestDeletePropertyNoInheritance(t *testing.T) {
+	actionChan := make(chan *Action, 1024)
+	errChan := make(chan *Error, 1024)
+
+	treeId := `10001000-1000-4000-1000-100010001000`
+	repoId := `20002000-2000-4000-2000-200020002000`
+	propId := `30003000-3000-4000-3000-300030003000`
+	teamId := `40004000-4000-4000-4000-400040004000`
+	buckId := `50005000-5000-4000-5000-500050005000`
+	grupId := `60006000-6000-4000-6000-600060006000`
+	cltrId := `70007000-7000-4000-7000-700070007000`
+	nodeId := `80008000-8000-4000-8000-800080008000`
+
+	propUUID, _ := uuid.FromString(propId)
+
+	sTree := New(TreeSpec{
+		Id:     treeId,
+		Name:   `root_testing`,
+		Action: actionChan,
+	})
+	NewRepository(RepositorySpec{
+		Id:      repoId,
+		Name:    `testrepo`,
+		Team:    teamId,
+		Deleted: false,
+		Active:  true,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: `root`,
+		ParentId:   treeId,
+	})
+	sTree.SetError(errChan)
+
+	sTree.Find(FindRequest{
+		ElementType: `repository`,
+		ElementId:   repoId,
+	}, true).(Propertier).SetProperty(&PropertySystem{
+		Id:           propUUID,
+		Inheritance:  false,
+		ChildrenOnly: false,
+		View:         `testview`,
+		Key:          `testkey`,
+		Value:        `testvalue`,
+	})
+
+	// create bucket
+	NewBucket(BucketSpec{
+		Id:          buckId,
+		Name:        `testrepo_test`,
+		Environment: `testing`,
+		Team:        teamId,
+		Deleted:     false,
+		Frozen:      false,
+		Repository:  repoId,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: "repository",
+		ParentId:   `repoId`,
+		ParentName: `testrepo`,
+	})
+
+	// create group
+	NewGroup(GroupSpec{
+		Id:   grupId,
+		Name: `testgroup`,
+		Team: teamId,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: `bucket`,
+		ParentId:   buckId,
+	})
+
+	// create cluster
+	NewCluster(ClusterSpec{
+		Id:   cltrId,
+		Name: `testcluster`,
+		Team: teamId,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: `group`,
+		ParentId:   grupId,
+	})
+
+	// assign node
+	NewNode(NodeSpec{
+		Id:       nodeId,
+		AssetId:  1,
+		Name:     `testnode`,
+		Team:     teamId,
+		ServerId: `00000000-0000-0000-0000-000000000000`,
+		Online:   true,
+		Deleted:  false,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: `cluster`,
+		ParentId:   cltrId,
+	})
+
+	sTree.Find(FindRequest{
+		ElementType: `repository`,
+		ElementId:   repoId,
+	}, true).(Propertier).DeleteProperty(&PropertySystem{
+		SourceId: propUUID,
+		View:     `testview`,
+		Key:      `testkey`,
+		Value:    `testvalue`,
+	})
+
+	close(actionChan)
+	close(errChan)
+
+	if len(errChan) != 0 {
+		t.Error(
+			`Expected 0 actions in errorChan, got`,
+			len(errChan),
+		)
+	}
+
+	if len(actionChan) != 11 {
+		t.Error(
+			`Expected 11 actions in actionChan, got`,
+			len(actionChan),
+		)
+	}
+
+	elem := 0
+	actions := [][]string{
+		[]string{`repository`, `create`},
+		[]string{`fault`, `create`},
+		[]string{`errorchannel`, `attached`},
+		[]string{`repository`, `property_new`},
+		[]string{`bucket`, `create`},
+		[]string{`group`, `create`},
+		[]string{`group`, `member_new`},
+		[]string{`cluster`, `create`},
+		[]string{`cluster`, `member_new`},
+		[]string{`node`, `update`},
+		[]string{`repository`, `property_delete`},
+	}
+	for a := range actionChan {
+		if a.Type != actions[elem][0] || a.Action != actions[elem][1] {
+			t.Error(
+				`Received incorrect action. Expected`,
+				actions[elem][0], actions[elem][1],
+				`and received`, a.Type, a.Action,
+			)
+		}
+		elem++
+	}
+
+	if len(sTree.Child.PropertySystem) != 0 {
+		t.Error(
+			`Exptected repository to have 0 system property, found`,
+			len(sTree.Child.PropertySystem),
+		)
+	}
+
+	if len(sTree.Child.Children[buckId].(*Bucket).PropertySystem) != 0 {
+		t.Error(
+			`Expected bucket to have 0 system property, found`,
+			len(sTree.Child.Children[buckId].(*Bucket).PropertySystem),
+		)
+	}
+
+	if len(sTree.Find(FindRequest{
+		ElementType: `group`,
+		ElementId:   grupId,
+	}, true).(*Group).PropertySystem) != 0 {
+		t.Error(
+			`Group has wrong system property count`,
+		)
+	}
+
+	if len(sTree.Find(FindRequest{
+		ElementType: `cluster`,
+		ElementId:   cltrId,
+	}, true).(*Cluster).PropertySystem) != 0 {
+		t.Error(
+			`Cluster has wrong system property count`,
+		)
+	}
+
+	if len(sTree.Find(FindRequest{
+		ElementType: `node`,
+		ElementId:   nodeId,
+	}, true).(*Node).PropertySystem) != 0 {
+		t.Error(
+			`Node has wrong system property count`,
+		)
+	}
+}
+
 func TestOverwriteProperty(t *testing.T) {
 	actionChan := make(chan *Action, 1024)
 	errChan := make(chan *Error, 1024)
