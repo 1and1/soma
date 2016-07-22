afd96bc8a07402cb877a04dfc0b679ea5ae38f68
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-07-22 00:12:21 +0000

    Test: bucket duplicate property detection

--- propertier_test.go
+++ propertier_test.go
@@ -3729,4 +3729,153 @@ func TestCloneAfterProperty(t *testing.T) {
 	}
 }
 
+func TestSetPropertyDuplicateDetectOnBucket(t *testing.T) {
+	actionChan := make(chan *Action, 1024)
+	errChan := make(chan *Error, 1024)
+
+	treeId := `10001000-1000-4000-1000-100010001000`
+	repoId := `20002000-2000-4000-2000-200020002000`
+	propId := `30003000-3000-4000-3000-300030003000`
+	teamId := `40004000-4000-4000-4000-400040004000`
+	buckId := `50005000-5000-4000-5000-500050005000`
+	dupeId := `99999999-9999-4999-9999-999999999999`
+
+	propUUID, _ := uuid.FromString(propId)
+	dupeUUID, _ := uuid.FromString(dupeId)
+
+	// create tree
+	sTree := New(TreeSpec{
+		Id:     treeId,
+		Name:   `root_testing`,
+		Action: actionChan,
+	})
+
+	// create repository
+	NewRepository(RepositorySpec{
+		Id:      repoId,
+		Name:    `testrepo`,
+		Team:    teamId,
+		Deleted: false,
+		Active:  true,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: `root`,
+		ParentId:   treeId,
+	})
+	sTree.SetError(errChan)
+
+	// create bucket
+	NewBucket(BucketSpec{
+		Id:          buckId,
+		Name:        `testrepo_test`,
+		Environment: `testing`,
+		Team:        teamId,
+		Deleted:     false,
+		Frozen:      false,
+		Repository:  repoId,
+	}).Attach(AttachRequest{
+		Root:       sTree,
+		ParentType: "repository",
+		ParentId:   `repoId`,
+		ParentName: `testrepo`,
+	})
+
+	// set property on bucket
+	sTree.Find(FindRequest{
+		ElementType: `bucket`,
+		ElementId:   buckId,
+	}, true).(Propertier).SetProperty(&PropertySystem{
+		Id:           propUUID,
+		Inheritance:  true,
+		ChildrenOnly: false,
+		View:         `testview`,
+		Key:          `testkey`,
+		Value:        `testvalue`,
+	})
+
+	// set property on bucket, again
+	sTree.Find(FindRequest{
+		ElementType: `bucket`,
+		ElementId:   buckId,
+	}, true).(Propertier).SetProperty(&PropertySystem{
+		Id:           dupeUUID,
+		Inheritance:  true,
+		ChildrenOnly: false,
+		View:         `testview`,
+		Key:          `testkey`,
+		Value:        `testvalue`,
+	})
+
+	close(actionChan)
+	close(errChan)
+
+	if len(errChan) != 1 {
+		t.Error(
+			`Expected 1 actions in errorChan, got`,
+			len(errChan),
+		)
+	}
+
+	if len(actionChan) != 5 {
+		t.Error(
+			`Expected 5 actions in actionChan, got`,
+			len(actionChan),
+		)
+	}
+
+	elem := 0
+	actions := [][]string{
+		[]string{`repository`, `create`},
+		[]string{`fault`, `create`},
+		[]string{`errorchannel`, `attached`},
+		[]string{`bucket`, `create`},
+		[]string{`bucket`, `property_new`},
+		[]string{`group`, `create`},
+		[]string{`group`, `property_new`},
+		[]string{`group`, `member_new`},
+		[]string{`cluster`, `create`},
+		[]string{`cluster`, `property_new`},
+		[]string{`cluster`, `member_new`},
+		[]string{`node`, `update`},
+		[]string{`node`, `property_new`},
+	}
+	for a := range actionChan {
+		if a.Type != actions[elem][0] || a.Action != actions[elem][1] {
+			t.Error(
+				`Received incorrect action. Expected`,
+				actions[elem][0], actions[elem][1],
+				`and received`, a.Type, a.Action,
+			)
+		}
+		elem++
+	}
+
+	if len(sTree.Child.PropertySystem) != 0 {
+		t.Error(
+			`Exptected 0 system property, got `,
+			len(sTree.Child.PropertySystem),
+		)
+	}
+
+	if len(sTree.Child.Children[buckId].(*Bucket).PropertySystem) != 1 {
+		t.Error(
+			`Expected bucket to have 1 system property, found`,
+			len(sTree.Child.Children[buckId].(*Bucket).PropertySystem),
+		)
+	}
+
+	for _, p := range sTree.Child.Children[buckId].(*Bucket).PropertySystem {
+		if p.GetSourceInstance() != propId {
+			t.Error(`Wrong source id`, p.GetSourceInstance())
+		}
+		if p.GetKey() != `testkey` {
+			t.Error(`Wrong key:`, p.GetKey())
+		}
+
+		if p.GetValue() != `testvalue` {
+			t.Error(`Wrong value:`, p.GetValue())
+		}
+	}
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
