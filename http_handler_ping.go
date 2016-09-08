5114f35ed4c7c9377ca73c8d529f231c801276b6
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-09-08 21:11:31 +0000

    Indicate Observer mode in HEAD requests

--- http_handler_ping.go
+++ http_handler_ping.go
@@ -11,10 +11,12 @@ func Ping(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 
 	w.Header().Set(`X-Powered-By`, `SOMA Configuration System`)
 	w.Header().Set(`X-Version`, SomaVersion)
-	switch SomaCfg.ReadOnly {
-	case true:
+	switch {
+	case SomaCfg.Observer == true:
+		w.Header().Set(`X-SOMA-Mode`, `Observer`)
+	case SomaCfg.ReadOnly == true:
 		w.Header().Set(`X-SOMA-Mode`, `ReadOnly`)
-	case false:
+	case SomaCfg.ReadOnly == false:
 		w.Header().Set(`X-SOMA-Mode`, `Master`)
 	}
 	w.WriteHeader(http.StatusNoContent)
