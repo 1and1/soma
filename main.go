d9e12a3a3ad124c666609616747f5e2b0810de4a
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2016-09-14 09:59:53 +0200

    Update version for 0.8.0 release

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "somaadm"
 	app.Usage = "SOMA Administrative Interface"
-	app.Version = "0.5.18"
+	app.Version = "0.8.0"
 	app.EnableBashCompletion = true
 
 	app = registerCommands(*app)
