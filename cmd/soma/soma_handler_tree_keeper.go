2486dff3eade430eae74e82a5e206f721aa138ae
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-10-11 19:21:39 +0000

    TreeKeeper: move tree updates to separate file

--- cmd/soma/soma_handler_tree_keeper.go
+++ cmd/soma/soma_handler_tree_keeper.go
@@ -272,135 +272,34 @@ func (tk *treeKeeper) process(q *treeRequest) {
 
 	// q.Action == `rebuild` will fall through switch
 	switch q.Action {
-	//
-	// REPOSITORY MANIPULATION REQUESTS
 
 	//
-	// BUCKET MANIPULATION REQUESTS
-	case "create_bucket":
-		tree.NewBucket(tree.BucketSpec{
-			Id:          uuid.NewV4().String(),
-			Name:        q.Bucket.Bucket.Name,
-			Environment: q.Bucket.Bucket.Environment,
-			Team:        tk.team,
-			Deleted:     q.Bucket.Bucket.IsDeleted,
-			Frozen:      q.Bucket.Bucket.IsFrozen,
-			Repository:  q.Bucket.Bucket.RepositoryId,
-		}).Attach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "repository",
-			ParentId:   tk.repoId,
-			ParentName: tk.repoName,
-		})
+	// TREE MANIPULATION REQUESTS
+	case
+		`create_bucket`:
+		tk.treeBucket(q)
 
-	//
-	// GROUP MANIPULATION REQUESTS
-	case "create_group":
-		tree.NewGroup(tree.GroupSpec{
-			Id:   uuid.NewV4().String(),
-			Name: q.Group.Group.Name,
-			Team: tk.team,
-		}).Attach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "bucket",
-			ParentId:   q.Group.Group.BucketId,
-		})
-	case "delete_group":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "group",
-			ElementId:   q.Group.Group.Id,
-		}, true).(tree.BucketAttacher).Destroy()
-	case "reset_group_to_bucket":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "group",
-			ElementId:   q.Group.Group.Id,
-		}, true).(tree.BucketAttacher).Detach()
-	case "add_group_to_group":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "group",
-			ElementId:   (*q.Group.Group.MemberGroups)[0].Id,
-		}, true).(tree.BucketAttacher).ReAttach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "group",
-			ParentId:   q.Group.Group.Id,
-		})
+	case
+		`create_group`,
+		`delete_group`,
+		`reset_group_to_bucket`,
+		`add_group_to_group`:
+		tk.treeGroup(q)
 
-	//
-	// CLUSTER MANIPULATION REQUESTS
-	case "create_cluster":
-		tree.NewCluster(tree.ClusterSpec{
-			Id:   uuid.NewV4().String(),
-			Name: q.Cluster.Cluster.Name,
-			Team: tk.team,
-		}).Attach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "bucket",
-			ParentId:   q.Cluster.Cluster.BucketId,
-		})
-	case "delete_cluster":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "cluster",
-			ElementId:   q.Cluster.Cluster.Id,
-		}, true).(tree.BucketAttacher).Destroy()
-	case "reset_cluster_to_bucket":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "cluster",
-			ElementId:   q.Cluster.Cluster.Id,
-		}, true).(tree.BucketAttacher).Detach()
-	case "add_cluster_to_group":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "cluster",
-			ElementId:   (*q.Group.Group.MemberClusters)[0].Id,
-		}, true).(tree.BucketAttacher).ReAttach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "group",
-			ParentId:   q.Group.Group.Id,
-		})
+	case
+		`create_cluster`,
+		`delete_cluster`,
+		`reset_cluster_to_bucket`,
+		`add_cluster_to_group`:
+		tk.treeCluster(q)
 
-	//
-	// NODE MANIPULATION REQUESTS
-	case "assign_node":
-		tree.NewNode(tree.NodeSpec{
-			Id:       q.Node.Node.Id,
-			AssetId:  q.Node.Node.AssetId,
-			Name:     q.Node.Node.Name,
-			Team:     q.Node.Node.TeamId,
-			ServerId: q.Node.Node.ServerId,
-			Online:   q.Node.Node.IsOnline,
-			Deleted:  q.Node.Node.IsDeleted,
-		}).Attach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "bucket",
-			ParentId:   q.Node.Node.Config.BucketId,
-		})
-	case "delete_node":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "node",
-			ElementId:   q.Node.Node.Id,
-		}, true).(tree.BucketAttacher).Destroy()
-	case "reset_node_to_bucket":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "node",
-			ElementId:   q.Node.Node.Id,
-		}, true).(tree.BucketAttacher).Detach()
-	case "add_node_to_group":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "node",
-			ElementId:   (*q.Group.Group.MemberNodes)[0].Id,
-		}, true).(tree.BucketAttacher).ReAttach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "group",
-			ParentId:   q.Group.Group.Id,
-		})
-	case "add_node_to_cluster":
-		tk.tree.Find(tree.FindRequest{
-			ElementType: "node",
-			ElementId:   (*q.Cluster.Cluster.Members)[0].Id,
-		}, true).(tree.BucketAttacher).ReAttach(tree.AttachRequest{
-			Root:       tk.tree,
-			ParentType: "cluster",
-			ParentId:   q.Cluster.Cluster.Id,
-		})
+	case
+		"assign_node",
+		"delete_node",
+		"reset_node_to_bucket",
+		"add_node_to_group",
+		"add_node_to_cluster":
+		tk.treeNode(q)
 
 	//
 	// PROPERTY MANIPULATION REQUESTS
