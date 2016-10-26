50e6998db53e1e42dd0b5460318360988a7338e5
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-10-26 22:39:07 +0000

    Proto update: add []CheckConfig to Details
    struct Details is used by Repository, Bucket, Group,
    Cluster and Node. This change allows `somaadm $x show`
    to display the checks on that object.

--- lib/proto/details.go
+++ lib/proto/details.go
@@ -1,9 +1,10 @@
 package proto
 
 type Details struct {
-	CreatedAt string `json:"createdAt,omitempty"`
-	CreatedBy string `json:"createdBy,omitempty"`
-	Server    Server `json:"server,omitempty"`
+	CreatedAt    string         `json:"createdAt,omitempty"`
+	CreatedBy    string         `json:"createdBy,omitempty"`
+	Server       Server         `json:"server,omitempty"`
+	CheckConfigs *[]CheckConfig `json:"checkConfigs,omitempty"`
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
