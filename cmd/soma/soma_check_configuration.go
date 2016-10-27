9ce25e06ec32041f533bc915270bb21e155f3662
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-10-27 20:50:25 +0000

    Add functions to export CheckConfig constraints
    These together with the functions from 0aa0d29d
    allow to build a complete proto.CheckConfig
    object.

--- cmd/soma/soma_check_configuration.go
+++ cmd/soma/soma_check_configuration.go
@@ -7,6 +7,7 @@ import (
 	"github.com/1and1/soma/lib/proto"
 )
 
+// expects stmt.CheckConfigShowBase as prepared statement
 func exportCheckConfig(prepStmt *sql.Stmt, queryId string) (
 	*proto.CheckConfig, error) {
 
@@ -63,6 +64,7 @@ func exportCheckConfig(prepStmt *sql.Stmt, queryId string) (
 	return &checkConfig, nil
 }
 
+// expects stmt.CheckConfigShowThreshold as prepared statement
 func exportCheckConfigThresholds(prepStmt *sql.Stmt, queryId string) (
 	[]proto.CheckConfigThreshold, error) {
 
@@ -111,6 +113,309 @@ func exportCheckConfigThresholds(prepStmt *sql.Stmt, queryId string) (
 	}
 
 	return thresholds, nil
+}
+
+// expects in that order:
+// - stmt.CheckConfigShowConstrCustom
+// - stmt.CheckConfigShowConstrSystem
+// - stmt.CheckConfigShowConstrNative
+// - stmt.CheckConfigShowConstrService
+// - stmt.CheckConfigShowConstrAttribute
+// - stmt.CheckConfigShowConstrOncall
+func exportCheckConfigConstraints(stmtCustom, stmtSystem,
+	stmtNative, stmtService, stmtAttribute, stmtOncall *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var err error
+	constraints := make([]proto.CheckConfigConstraint, 0)
+	for _, cstrType := range []string{
+		`custom`,
+		`system`,
+		`native`,
+		`service`,
+		`attribute`,
+		`oncall`,
+	} {
+		cstr := []proto.CheckConfigConstraint{}
+		switch cstrType {
+		case `custom`:
+			cstr, err = exportCheckConfigCustomConstraints(
+				stmtCustom, queryId)
+		case `system`:
+			cstr, err = exportCheckConfigSystemConstraints(
+				stmtSystem, queryId)
+		case `native`:
+			cstr, err = exportCheckConfigNativeConstraints(
+				stmtNative, queryId)
+		case `service`:
+			cstr, err = exportCheckConfigServiceConstraints(
+				stmtService, queryId)
+		case `attribute`:
+			cstr, err = exportCheckConfigAttributeConstraints(
+				stmtAttribute, queryId)
+		case `oncall`:
+			cstr, err = exportCheckConfigOncallConstraints(
+				stmtOncall, queryId)
+		}
+		if err != nil {
+			return nil, err
+		}
+		constraints = append(constraints, cstr...)
+	}
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrCustom as prepared statement
+func exportCheckConfigCustomConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, propertyId, repositoryId string
+		name, value                             string
+		rows                                    *sql.Rows
+		err                                     error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&propertyId,
+			&repositoryId,
+			&value,
+			&name,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `custom`,
+			Custom: &proto.PropertyCustom{
+				Id:           propertyId,
+				RepositoryId: repositoryId,
+				Name:         name,
+				Value:        value,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrSystem as prepared statement
+func exportCheckConfigSystemConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, name, value string
+		rows                       *sql.Rows
+		err                        error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&name,
+			&value,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `system`,
+			System: &proto.PropertySystem{
+				Name:  name,
+				Value: value,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrNative as prepared statement
+func exportCheckConfigNativeConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, name, value string
+		rows                       *sql.Rows
+		err                        error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&name,
+			&value,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `native`,
+			Native: &proto.PropertyNative{
+				Name:  name,
+				Value: value,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrService as prepared statement
+func exportCheckConfigServiceConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, name, teamId string
+		rows                        *sql.Rows
+		err                         error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&teamId,
+			&name,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `service`,
+			Service: &proto.PropertyService{
+				Name:   name,
+				TeamId: teamId,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrAttribute as prepared statement
+func exportCheckConfigAttributeConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, name, value string
+		rows                       *sql.Rows
+		err                        error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&name,
+			&value,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `attribute`,
+			Attribute: &proto.ServiceAttribute{
+				Name:  name,
+				Value: value,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
+}
+
+// expects stmt.CheckConfigShowConstrOncall as prepared statement
+func exportCheckConfigOncallConstraints(prepStmt *sql.Stmt,
+	queryId string) ([]proto.CheckConfigConstraint, error) {
+
+	var (
+		checkConfigId, oncallId, name, number string
+		rows                                  *sql.Rows
+		err                                   error
+	)
+
+	constraints := make([]proto.CheckConfigConstraint, 0)
+
+	if rows, err = prepStmt.Query(queryId); err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&checkConfigId,
+			&oncallId,
+			&name,
+			&number,
+		); err != nil {
+			rows.Close()
+			return nil, err
+		}
+		cstr := proto.CheckConfigConstraint{
+			ConstraintType: `oncall`,
+			Oncall: &proto.PropertyOncall{
+				Id:     oncallId,
+				Name:   name,
+				Number: number,
+			},
+		}
+		constraints = append(constraints, cstr)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return constraints, nil
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
