4d821684dd1ca6fea61bc9a4a4cbe80f674f56b3
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-10-31 22:10:17 +0000

    proto: add missing copyright headers

--- lib/proto/mode.go
+++ lib/proto/mode.go
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (c) 2016, 1&1 Internet SE
+ * Copyright (c) 2016, Jörg Pernfuß <joerg.pernfuss@1und1.de>
+ * All rights reserved
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
 package proto
 
 type Mode struct {
