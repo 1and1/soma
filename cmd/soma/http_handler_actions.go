e80fa0606a7627e9f39ab0f41b72c60809ed9dba
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-11-12 00:17:19 +0000

    soma/http: add support for Sections, Actions
    This commit adds List and Show targets for
    Sections and Actions.

---
+++ cmd/soma/http_handler_actions.go
@@ -0,0 +1,83 @@
+/*-
+ * Copyright (c) 2016, 1&1 Internet SE
+ * Copyright (c) 2016, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/1and1/soma/internal/msg"
+	"github.com/1and1/soma/lib/proto"
+	"github.com/julienschmidt/httprouter"
+)
+
+// ActionList accepts requests to list actions in a specific section
+func ActionList(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `action`,
+		Action:     `list`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `action`,
+		Action:     `list`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		Action: proto.Action{
+			SectionId: params.ByName(`section`),
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// ActionShow accepts requests to show details about a specific
+// action
+func ActionShow(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `action`,
+		Action:     `show`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `action`,
+		Action:     `show`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		Action: proto.Action{
+			Id:        params.ByName(`action`),
+			SectionId: params.ByName(`section`),
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ cmd/soma/http_handler_sections.go
@@ -0,0 +1,79 @@
+/*-
+ * Copyright (c) 2016, 1&1 Internet SE
+ * Copyright (c) 2016, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/1and1/soma/internal/msg"
+	"github.com/1and1/soma/lib/proto"
+	"github.com/julienschmidt/httprouter"
+)
+
+// SectionList accepts requests to list all sections
+func SectionList(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `section`,
+		Action:     `list`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `section`,
+		Action:     `list`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// SectionShow accepts requests to show details about a specific
+// section
+func SectionShow(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `section`,
+		Action:     `show`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `section`,
+		Action:     `show`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		Section: proto.Section{
+			Id: params.ByName(`section`),
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
