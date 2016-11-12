f960086caf64f3e78a75b43e4e440dbfc294cc89
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-11-12 21:45:43 +0000

    soma/http: add remaining action endpoints
    This commit adds:
    - ActionSearch
    - ActionAdd
    - ActionRemove
    
    to the rest layer and hooks up the routes.

--- cmd/soma/http_handler_actions.go
+++ cmd/soma/http_handler_actions.go
@@ -9,6 +9,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/1and1/soma/internal/msg"
@@ -68,6 +69,127 @@ func ActionShow(w http.ResponseWriter, r *http.Request,
 	handler.input <- msg.Request{
 		Section:    `action`,
 		Action:     `show`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		ActionObj: proto.Action{
+			Id:        params.ByName(`action`),
+			SectionId: params.ByName(`section`),
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+func ActionSearch(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `action`,
+		Action:     `search`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	cReq := proto.Request{}
+	err := DecodeJsonBody(r, &cReq)
+	if err != nil {
+		DispatchBadRequest(&w, err)
+		return
+	}
+
+	if cReq.Action.SectionId == `` || cReq.Action.Name == `` {
+		DispatchBadRequest(&w,
+			fmt.Errorf(`Invalid action search specification`))
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `action`,
+		Action:     `search`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		ActionObj: proto.Action{
+			Name:      cReq.Action.Name,
+			SectionId: cReq.Action.SectionId,
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// ActionAdd accepts requests to add a new action to a section
+func ActionAdd(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `action`,
+		Action:     `add`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	cReq := proto.Request{}
+	err := DecodeJsonBody(r, &cReq)
+	if err != nil {
+		DispatchBadRequest(&w, err)
+		return
+	}
+
+	if cReq.Action.SectionId != params.ByName(`section`) {
+		DispatchBadRequest(&w, fmt.Errorf("SectionId mismatch: %s, %s",
+			cReq.Action.SectionId, params.ByName(`section`)))
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `action`,
+		Action:     `add`,
+		Reply:      returnChannel,
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		User:       params.ByName(`AuthenticatedUser`),
+		ActionObj: proto.Action{
+			Name:      cReq.Action.Name,
+			SectionId: cReq.Action.SectionId,
+		},
+	}
+	result := <-returnChannel
+	SendMsgResult(&w, &result)
+}
+
+// ActionRemove accepts requests to remove an action form a section
+func ActionRemove(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	defer PanicCatcher(w)
+
+	if !IsAuthorizedd(&msg.Authorization{
+		User:       params.ByName(`AuthenticatedUser`),
+		RemoteAddr: extractAddress(r.RemoteAddr),
+		Section:    `action`,
+		Action:     `remove`,
+	}) {
+		DispatchForbidden(&w, nil)
+		return
+	}
+
+	returnChannel := make(chan msg.Result)
+	handler := handlerMap[`supervisor`].(*supervisor)
+	handler.input <- msg.Request{
+		Section:    `action`,
+		Action:     `remove`,
 		Reply:      returnChannel,
 		RemoteAddr: extractAddress(r.RemoteAddr),
 		User:       params.ByName(`AuthenticatedUser`),
