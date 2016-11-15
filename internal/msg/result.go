2523f57781ff6e29bdc85885ba4b1b31c3dac6c9
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2016-11-15 02:09:10 +0000

    msg: add Result.UnknownRequest()
    A convenience-wrapper for .NotImplemented()

--- internal/msg/result.go
+++ internal/msg/result.go
@@ -146,4 +146,9 @@ func (r *Result) Unavailable(err error) {
 	r.SetError(err)
 }
 
+func (r *Result) UnknownRequest(q *Request) {
+	r.NotImplemented(fmt.Errorf("Unknown requested action:"+
+		" %s/%s", q.Section, q.Action))
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
