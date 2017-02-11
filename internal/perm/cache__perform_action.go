4fd7ecd1f1db267d83a0beef17c52061d2c7d482
Jörg Pernfuß <joerg.pernfuss@1und1.de>
2017-02-11 16:08:58 +0000

    perm: order action methods

--- internal/perm/cache__perform_action.go
+++ internal/perm/cache__perform_action.go
@@ -11,10 +11,19 @@ import "github.com/1and1/soma/internal/msg"
 
 // These are the per-Action methods used in Cache.Perform
 
-func (c *Cache) performRepositoryCreate(q *msg.Request) {
+func (c *Cache) performActionAdd(q *msg.Request) {
+	c.lock.Lock()
+	c.action.add(
+		q.ActionObj.SectionId,
+		q.ActionObj.SectionName,
+		q.ActionObj.Id,
+		q.ActionObj.Name,
+		q.ActionObj.Category,
+	)
+	c.lock.Unlock()
 }
 
-func (c *Cache) performRepositoryDestroy(q *msg.Request) {
+func (c *Cache) performActionRemove(q *msg.Request) {
 }
 
 func (c *Cache) performBucketCreate(q *msg.Request) {
@@ -23,10 +32,7 @@ func (c *Cache) performBucketCreate(q *msg.Request) {
 func (c *Cache) performBucketDestroy(q *msg.Request) {
 }
 
-func (c *Cache) performGroupCreate(q *msg.Request) {
-}
-
-func (c *Cache) performGroupDestroy(q *msg.Request) {
+func (c *Cache) performCategoryRemove(q *msg.Request) {
 }
 
 func (c *Cache) performClusterCreate(q *msg.Request) {
@@ -35,10 +41,62 @@ func (c *Cache) performClusterCreate(q *msg.Request) {
 func (c *Cache) performClusterDestroy(q *msg.Request) {
 }
 
+func (c *Cache) performGroupCreate(q *msg.Request) {
+}
+
+func (c *Cache) performGroupDestroy(q *msg.Request) {
+}
+
 func (c *Cache) performNodeAssign(q *msg.Request) {
 }
 
 func (c *Cache) performNodeUnassign(q *msg.Request) {
+}
+
+func (c *Cache) performPermissionMap(q *msg.Request) {
+}
+
+func (c *Cache) performPermissionRemove(q *msg.Request) {
+}
+
+func (c *Cache) performPermissionUnmap(q *msg.Request) {
+}
+
+func (c *Cache) performRepositoryCreate(q *msg.Request) {
+}
+
+func (c *Cache) performRepositoryDestroy(q *msg.Request) {
+}
+
+func (c *Cache) performRightGrant(q *msg.Request) {
+}
+
+func (c *Cache) performRightRevoke(q *msg.Request) {
+}
+
+func (c *Cache) performSectionAdd(q *msg.Request) {
+	c.lock.Lock()
+	c.section.add(
+		q.SectionObj.Id,
+		q.SectionObj.Name,
+		q.SectionObj.Category,
+	)
+	c.lock.Unlock()
+}
+
+func (c *Cache) performSectionRemove(q *msg.Request) {
+}
+
+func (c *Cache) performTeamAdd(q *msg.Request) {
+	c.lock.Lock()
+	c.team.add(
+		q.Team.Id,
+		q.Team.Name,
+	)
+	c.lock.Unlock()
+}
+
+func (c *Cache) performTeamRemove(q *msg.Request) {
 }
 
 func (c *Cache) performUserAdd(q *msg.Request) {
@@ -56,64 +114,6 @@ func (c *Cache) performUserAdd(q *msg.Request) {
 }
 
 func (c *Cache) performUserRemove(q *msg.Request) {
-}
-
-func (c *Cache) performTeamAdd(q *msg.Request) {
-	c.lock.Lock()
-	c.team.add(
-		q.Team.Id,
-		q.Team.Name,
-	)
-	c.lock.Unlock()
-}
-
-func (c *Cache) performTeamRemove(q *msg.Request) {
-}
-
-func (c *Cache) performRightGrant(q *msg.Request) {
-}
-
-func (c *Cache) performRightRevoke(q *msg.Request) {
-}
-
-func (c *Cache) performPermissionRemove(q *msg.Request) {
-}
-
-func (c *Cache) performPermissionMap(q *msg.Request) {
-}
-
-func (c *Cache) performPermissionUnmap(q *msg.Request) {
-}
-
-func (c *Cache) performCategoryRemove(q *msg.Request) {
-}
-
-func (c *Cache) performSectionAdd(q *msg.Request) {
-	c.lock.Lock()
-	c.section.add(
-		q.SectionObj.Id,
-		q.SectionObj.Name,
-		q.SectionObj.Category,
-	)
-	c.lock.Unlock()
-}
-
-func (c *Cache) performSectionRemove(q *msg.Request) {
-}
-
-func (c *Cache) performActionAdd(q *msg.Request) {
-	c.lock.Lock()
-	c.action.add(
-		q.ActionObj.SectionId,
-		q.ActionObj.SectionName,
-		q.ActionObj.Id,
-		q.ActionObj.Name,
-		q.ActionObj.Category,
-	)
-	c.lock.Unlock()
-}
-
-func (c *Cache) performActionRemove(q *msg.Request) {
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
