f63d9389f5cd7190a40406219a62e321753eb36d
Pernfuß, Jörg <joerg.pernfuss@1und1.de>
2017-04-28 09:18:26 +0200

    Improve startup logging of drained channels
    If configuration key startup.print.channel.errors is true, also
    print the drained contents of the action channels.

--- cmd/soma/soma_handler_tree_keeper__util.go
+++ cmd/soma/soma_handler_tree_keeper__util.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+)
+
 func (tk *treeKeeper) drain(s string) (j int) {
 	switch s {
 	case "action":
 		j = len(tk.actionChan)
 		for i := j; i > 0; i-- {
+			if SomaCfg.PrintChannels {
+				a := <-tk.actionChan
+				jBxX, _ := json.Marshal(a)
+				log.Printf("TK[%s], drained: %s", tk.repoName, string(jBxX))
+				continue
+			}
 			<-tk.actionChan
 		}
 	case "error":
