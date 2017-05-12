1and1/soma

commit c5be7977876741f8773388c791c19815b4f59581

eye: check empty Service.Name for Metadata.Source

cmd/eye/util.go
@@ -112,7 +112,7 @@ func Itemize(details *proto.Deployment) (string, *ConfigurationItem, error) {
 	}
 
 	// construct item.Metadata.Source
-	if details.Service != nil {
+	if details.Service != nil && details.Service != `` {
 		item.Metadata.Source = fmt.Sprintf("%s, %s", details.Service.Name, details.CheckConfig.Name)
 	} else {
 		item.Metadata.Source = fmt.Sprintf("System (%s), %s", details.Node.Name, details.CheckConfig.Name)

commit 3ddbf0c2ffff80102f85dec2f6e71908494d8592

eye: fix check empty Service.Name
This commit fixes an incorrect check introduced
in  c5be7977876741f8773388c791c19815b4f59581

cmd/eye/util.go
@@ -112,7 +112,7 @@ func Itemize(details *proto.Deployment) (string, *ConfigurationItem, error) {
 	}
 
 	// construct item.Metadata.Source
-	if details.Service != nil && details.Service != `` {
+	if details.Service != nil && details.Service.Name != `` {
 		item.Metadata.Source = fmt.Sprintf("%s, %s", details.Service.Name, details.CheckConfig.Name)
 	} else {
 		item.Metadata.Source = fmt.Sprintf("System (%s), %s", details.Node.Name, details.CheckConfig.Name)